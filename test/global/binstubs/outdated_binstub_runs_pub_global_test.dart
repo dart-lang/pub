9811f96f81e64110745cae56baf31fde65b97972
nweiz@google.com <nweiz@google.com>
2014-12-01 23:44:49 +0000

    Make pub's binstubs resilient to changes in snapshot format.
    This also makes "pub cache repair" repair activated packages, including updating
    the binstub format.
    
    R=rnystrom@google.com
    BUG=21463
    
    Review URL: https://codereview.chromium.org//745153002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@42055 260f80e4-7a28-3924-810f-c04153c831b5

--- test/global/binstubs/outdated_binstub_runs_pub_global_test.dart
+++ test/global/binstubs/outdated_binstub_runs_pub_global_test.dart
@@ -2,14 +2,20 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:io';
+
+import 'package:path/path.dart' as p;
+import 'package:scheduled_test/scheduled_process.dart';
+import 'package:scheduled_test/scheduled_stream.dart';
 import 'package:scheduled_test/scheduled_test.dart';
 
 import '../../descriptor.dart' as d;
 import '../../test_pub.dart';
+import 'utils.dart';
 
 main() {
   initConfig();
-  integration("the binstubs runs a precompiled snapshot if present", () {
+  integration("an outdated binstub runs 'pub global run'", () {
     servePackages((builder) {
       builder.serve("foo", "1.0.0", pubspec: {
         "executables": {
@@ -25,10 +31,19 @@ main() {
     schedulePub(args: ["global", "activate", "foo"]);
 
     d.dir(cachePath, [
-      d.dir("bin", [
-        d.matcherFile(binStubName("foo-script"),
-            contains("script.dart.snapshot"))
+      d.dir('global_packages', [
+        d.dir('foo', [
+          d.dir('bin', [d.outOfDateSnapshot('script.dart.snapshot')])
+        ])
       ])
-    ]).validate();
+    ]).create();
+
+    var process = new ScheduledProcess.start(
+        p.join(sandboxDir, cachePath, "bin", binStubName("foo-script")),
+        ["arg1", "arg2"],
+        environment: getEnvironment());
+
+    process.stdout.expect(consumeThrough("ok"));
+    process.shouldExit();
   });
 }
