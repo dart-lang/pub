2da36d1b08f620cc8c5170fb80b7ba196eb9bbbc
Natalie Weizenbaum <nweiz@google.com>
2016-12-06 14:40:30 -0800

    Properly close stdin for child processes.
    Closes #25348.

--- test/run/app_can_read_from_stdin_test.dart
+++ test/run/app_can_read_from_stdin_test.dart
@@ -5,25 +5,23 @@
 import '../descriptor.dart' as d;
 import '../test_pub.dart';
 
-const SCRIPT = """
-import 'dart:io';
-
 main() {
-  print("started");
-  var line1 = stdin.readLineSync();
-  print("between");
-  var line2 = stdin.readLineSync();
-  print(line1);
-  print(line2);
-}
-""";
-
-main() {
-  integration('the spawned application can read from stdin', () {
+  integration('the spawned application can read line-by-line from stdin', () {
     d.dir(appPath, [
       d.appPubspec(),
       d.dir("bin", [
-        d.file("script.dart", SCRIPT)
+        d.file("script.dart", """
+          import 'dart:io';
+
+          main() {
+            print("started");
+            var line1 = stdin.readLineSync();
+            print("between");
+            var line2 = stdin.readLineSync();
+            print(line1);
+            print(line2);
+          }
+        """)
       ])
     ]).create();
 
@@ -36,6 +34,35 @@ main() {
     pub.writeLine("second");
     pub.stdout.expect("first");
     pub.stdout.expect("second");
+    pub.shouldExit(0);
+  });
+
+  integration('the spawned application can read streamed from stdin', () {
+    d.dir(appPath, [
+      d.appPubspec(),
+      d.dir("bin", [
+        d.file("script.dart", """
+          import 'dart:io';
+
+          main() {
+            print("started");
+            stdin.listen(stdout.add);
+          }
+        """)
+      ])
+    ]).create();
+
+    pubGet();
+    var pub = pubRun(args: ["bin/script"]);
+
+    pub.stdout.expect("started");
+    pub.writeLine("first");
+    pub.stdout.expect("first");
+    pub.writeLine("second");
+    pub.stdout.expect("second");
+    pub.writeLine("third");
+    pub.stdout.expect("third");
+    pub.closeStdin();
     pub.shouldExit(0);
   });
 }
