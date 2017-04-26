3f90a95c6426545e5a04bd5aa772f0f10a28b316
Jacob MacDonald <jakemac@google.com>
2017-04-26 10:04:27 -0700

    remove unused import

--- test/barback/dartdevc/module_computer_test.dart
+++ test/barback/dartdevc/module_computer_test.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:barback/barback.dart';
 import 'package:test/test.dart';
 
 import 'package:pub/src/barback/dartdevc/module_computer.dart';
