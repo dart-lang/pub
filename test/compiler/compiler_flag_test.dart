2813fcf016abeb85e20d9ca4f58a479227808fb8
Jacob MacDonald <jakemac@google.com>
2017-04-27 12:47:01 -0700

    add unit test, update compiler flag test

--- test/compiler/compiler_flag_test.dart
+++ test/compiler/compiler_flag_test.dart
@@ -4,6 +4,8 @@
 
 import 'package:scheduled_test/scheduled_test.dart';
 
+import 'package:pub/src/barback/dartdevc/module_reader.dart';
+
 import '../descriptor.dart' as d;
 import '../test_pub.dart';
 import '../serve/utils.dart';
@@ -14,18 +16,32 @@ main() {
       d.appPubspec(),
       d.dir("lib", [
         d.file("hello.dart", "hello() => print('hello');"),
-      ])
+      ]),
+      d.dir("web", [
+        d.file(
+            "main.dart",
+            '''
+          import 'package:myapp/hello.dart';
+
+          void main() => hello();
+        '''),
+      ]),
     ]).create();
 
     pubGet();
     pubServe(args: ['--compiler', 'dartdevc']);
     requestShouldSucceed(
-        'packages/$appPath/.moduleConfig', contains('lib__hello'));
-    // Binary response, just confirm it exists.
+        'packages/$appPath/$moduleConfigName', contains('lib__hello'));
+    requestShouldSucceed(moduleConfigName, contains('web__main'));
     requestShouldSucceed('packages/$appPath/lib__hello.unlinked.sum', null);
+    requestShouldSucceed('web__main.unlinked.sum', null);
     requestShouldSucceed('packages/$appPath/lib__hello.linked.sum', null);
-    // TODO(jakemac53): Not implemented yet, update once available.
-    requestShould404('packages/$appPath/lib__hello.js');
+    requestShouldSucceed('web__main.linked.sum', null);
+    requestShouldSucceed('packages/$appPath/lib__hello.js', contains('hello'));
+    requestShouldSucceed('web__main.js', contains('hello'));
+    // Bootstrap js file, should have the same name as the original dart file
+    // but with `.js` added.
+    requestShould404('main.dart.js');
     endPubServe();
   });
 }
