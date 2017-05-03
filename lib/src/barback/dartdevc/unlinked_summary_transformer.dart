0c294d33f0ed031cac64d0959b14c8d680bc177a
Jacob MacDonald <jakemac@google.com>
2017-05-03 16:53:10 -0700

    Add the UnlinkedSummaryTransformer and ScratchSpace classes (#1578)
    * add ScratchSpace class which manages creating temp environments for running external binaries
    * add analyzer worker driver and bazel_worker dependency
    * add the UnlinkedSummaryTransformer
    * rename modules after creation so the names arent so long
    * Add UnlinkedSummaryTransformer to packages, use file: uris for analyzer cli

---
+++ lib/src/barback/dartdevc/unlinked_summary_transformer.dart
@@ -0,0 +1,78 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:barback/barback.dart';
+import 'package:bazel_worker/bazel_worker.dart';
+import 'package:path/path.dart' as p;
+
+import '../../io.dart';
+import 'workers.dart';
+import 'module.dart';
+import 'module_reader.dart';
+import 'scratch_space.dart';
+
+final String unlinkedSummaryExtension = '.unlinked.sum';
+
+/// Creates unlinked analyzer summaries given [moduleConfigName] files which
+/// describe a set of [Module]s.
+class UnlinkedSummaryTransformer extends Transformer {
+  @override
+  String get allowedExtensions => moduleConfigName;
+
+  UnlinkedSummaryTransformer();
+
+  @override
+  Future apply(Transform transform) async {
+    var reader = new ModuleReader(transform.readInputAsString);
+    var configId = transform.primaryInput.id;
+    var modules = await reader.readModules(configId);
+    ScratchSpace scratchSpace;
+    try {
+      var allAssetIds = modules.fold(new Set<AssetId>(), (allAssets, module) {
+        allAssets.addAll(module.assetIds);
+        return allAssets;
+      });
+      // Create a single temp environment for all the modules in this package.
+      scratchSpace =
+          await ScratchSpace.create(allAssetIds, transform.readInput);
+      await Future.wait(modules.map((m) => _createUnlinkedSummaryForModule(
+          m, topLevelDir(configId.path), scratchSpace, transform)));
+    } finally {
+      scratchSpace?.delete();
+    }
+  }
+}
+
+Future _createUnlinkedSummaryForModule(Module module, String outputDir,
+    ScratchSpace scratchSpace, Transform transform) async {
+  var summaryOutputId = new AssetId(module.id.package,
+      p.url.join(outputDir, '${module.id.name}$unlinkedSummaryExtension'));
+  var summaryOutputFile = scratchSpace.fileFor(summaryOutputId);
+  var request = new WorkRequest();
+  request.arguments.addAll([
+    '--build-summary-only',
+    '--build-summary-only-unlinked',
+    '--build-summary-only-diet',
+    '--build-summary-output=${summaryOutputFile.path}',
+  ]);
+  // Add all the files to include in the unlinked summary bundle.
+  request.arguments.addAll(module.assetIds.map((id) {
+    var uri = canonicalUriFor(id);
+    if (!uri.startsWith('package:')) {
+      uri = 'file://$uri';
+    }
+    return '$uri|${scratchSpace.fileFor(id).path}';
+  }));
+  var response = await analyzerDriver.doWork(request);
+  if (response.exitCode == EXIT_CODE_ERROR) {
+    transform.logger
+        .error('Error creating unlinked summaries for module: ${module.id}.\n'
+            '${response.output}');
+  } else {
+    transform.addOutput(new Asset.fromBytes(
+        summaryOutputId, summaryOutputFile.readAsBytesSync()));
+  }
+}
