eeede1c882465276f1c0269cf14325a6f9061046
Jacob MacDonald <jakemac@google.com>
2017-05-03 17:33:21 -0700

    update to ScratchSpace class

--- lib/src/barback/dartdevc/dartdevc_module_transformer.dart
+++ lib/src/barback/dartdevc/dartdevc_module_transformer.dart
@@ -11,7 +11,7 @@ import 'package:path/path.dart' as p;
 import 'workers.dart';
 import 'module.dart';
 import 'module_reader.dart';
-import 'temp_environment.dart';
+import 'scratch_space.dart';
 import 'linked_summary_transformer.dart';
 
 /// Creates dartdevc modules given [moduleConfigName] files which describe a set
@@ -27,7 +27,7 @@ class DartDevcModuleTransformer extends Transformer {
 
   @override
   Future apply(Transform transform) async {
-    TempEnvironment tempEnv;
+    ScratchSpace scratchSpace;
     var reader = new ModuleReader(transform.readInputAsString);
     var modules = await reader.readModules(transform.primaryInput.id);
     try {
@@ -41,20 +41,21 @@ class DartDevcModuleTransformer extends Transformer {
         allAssetIds..addAll(module.assetIds)..addAll(linkedSummaryIds);
       }
       // Create a single temp environment for all the modules in this package.
-      tempEnv = await TempEnvironment.create(allAssetIds, transform.readInput);
+      scratchSpace =
+          await ScratchSpace.create(allAssetIds, transform.readInput);
       await Future.wait(modules.map((m) => _createDartdevcModule(
-          m, tempEnv, summariesForModule[m.id], transform)));
+          m, scratchSpace, summariesForModule[m.id], transform)));
     } finally {
-      tempEnv?.delete();
+      scratchSpace?.delete();
     }
   }
 }
 
 /// Compiles [module] using the `dartdevc` binary from the SDK to a relative
 /// path under the package that looks like `$outputDir/${module.id.name}.js`.
-Future _createDartdevcModule(Module module, TempEnvironment tempEnv,
+Future _createDartdevcModule(Module module, ScratchSpace scratchSpace,
     Set<AssetId> linkedSummaryIds, Transform transform) async {
-  var jsOutputFile = tempEnv.fileFor(module.id.jsId);
+  var jsOutputFile = scratchSpace.fileFor(module.id.jsId);
   var sdk_summary = p.url.join(sdkDir.path, 'lib/_internal/ddc_sdk.sum');
   var request = new WorkRequest();
   request.arguments.addAll([
@@ -64,7 +65,7 @@ Future _createDartdevcModule(Module module, TempEnvironment tempEnv,
     '--unsafe-angular2-whitelist',
     '--modules=amd',
     '--dart-sdk=${sdkDir.path}',
-    '--module-root=${tempEnv.tempDir.path}',
+    '--module-root=${scratchSpace.tempDir.path}',
     '--library-root=${p.dirname(jsOutputFile.path)}',
     '--summary-extension=${linkedSummaryExtension.substring(1)}',
     '-o',
@@ -72,14 +73,15 @@ Future _createDartdevcModule(Module module, TempEnvironment tempEnv,
   ]);
   // Add all the linked summaries as summary inputs.
   for (var id in linkedSummaryIds) {
-    request.arguments.addAll(['-s', tempEnv.fileFor(id).path]);
+    request.arguments.addAll(['-s', scratchSpace.fileFor(id).path]);
   }
   // Add URL mappings for all the package: files to tell DartDevc where to find
   // them.
   for (var id in module.assetIds) {
     var uri = canonicalUriFor(id);
     if (uri.startsWith('package:')) {
-      request.arguments.add('--url-mapping=$uri,${tempEnv.fileFor(id).path}');
+      request.arguments
+          .add('--url-mapping=$uri,${scratchSpace.fileFor(id).path}');
     }
   }
   // And finally add all the urls to compile, using the package: path for files
@@ -89,7 +91,7 @@ Future _createDartdevcModule(Module module, TempEnvironment tempEnv,
     if (uri.startsWith('package:')) {
       return uri;
     }
-    return tempEnv.fileFor(id).path;
+    return scratchSpace.fileFor(id).path;
   }));
 
   var response = await dartdevcDriver.doWork(request);
