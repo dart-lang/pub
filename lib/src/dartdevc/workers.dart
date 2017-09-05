02501828cf39e3f97676a48b8245ac77754a3345
Jacob MacDonald <jakemac@google.com>
2017-09-05 10:33:58 -0700

    Add support for PUB_MAX_WORKERS_PER_TASK system environment variable

--- lib/src/dartdevc/workers.dart
+++ lib/src/dartdevc/workers.dart
@@ -8,16 +8,31 @@ import 'package:bazel_worker/driver.dart';
 import 'package:cli_util/cli_util.dart' as cli_util;
 import 'package:path/path.dart' as p;
 
+import '../log.dart';
+
 String get _scriptExtension => Platform.isWindows ? '.bat' : '';
 
+const _defaultMaxWorkers = 4;
+
+const _maxWorkersEnvVar = 'PUB_MAX_WORKERS_PER_TASK';
+
+final int _maxWorkersPerTask = int.parse(
+    Platform.environment[_maxWorkersEnvVar] ?? '$_defaultMaxWorkers',
+    onError: (value) => warning(
+        yellow('Invalid value for $_maxWorkersEnvVar environment variable, '
+            'expected an int but got `$value`. Falling back to default value '
+            'of $_defaultMaxWorkers.')));
+
 /// Manages a shared set of persistent analyzer workers.
-final analyzerDriver = new BazelWorkerDriver(() => Process.start(
-    p.join(sdkDir, 'bin', 'dartanalyzer$_scriptExtension'),
-    ['--build-mode', '--persistent_worker']));
+final analyzerDriver = new BazelWorkerDriver(
+    () => Process.start(p.join(sdkDir, 'bin', 'dartanalyzer$_scriptExtension'),
+        ['--build-mode', '--persistent_worker']),
+    maxWorkers: _maxWorkersPerTask);
 
 /// Manages a shared set of persistent dartdevc workers.
-final dartdevcDriver = new BazelWorkerDriver(() => Process.start(
-    p.join(sdkDir, 'bin', 'dartdevc$_scriptExtension'),
-    ['--persistent_worker']));
+final dartdevcDriver = new BazelWorkerDriver(
+    () => Process.start(p.join(sdkDir, 'bin', 'dartdevc$_scriptExtension'),
+        ['--persistent_worker']),
+    maxWorkers: _maxWorkersPerTask);
 
 final sdkDir = cli_util.getSdkPath();
