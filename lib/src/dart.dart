0eed5a5595314ee73f83c2e33e397d8d86b08bb8
Nate Bosch <nbosch@google.com>
2018-04-16 17:05:46 -0700

    Removed unused dart.dart utilities

--- lib/src/dart.dart
+++ lib/src/dart.dart
@@ -29,10 +29,6 @@ bool isEntrypoint(CompilationUnit dart) {
   });
 }
 
-/// Returns whether [dart] contains a [PartOfDirective].
-bool isPart(CompilationUnit dart) =>
-    dart.directives.any((directive) => directive is PartOfDirective);
-
 /// Efficiently parses the import and export directives in [contents].
 ///
 /// If [name] is passed, it's used as the filename for error reporting.
@@ -47,59 +43,6 @@ class _DirectiveCollector extends GeneralizingAstVisitor {
   final directives = <UriBasedDirective>[];
 
   visitUriBasedDirective(UriBasedDirective node) => directives.add(node);
-}
-
-/// Runs [code] in an isolate.
-///
-/// [code] should be the contents of a Dart entrypoint. It may contain imports;
-/// they will be resolved in the same context as the host isolate. [message] is
-/// passed to the [main] method of the code being run; the caller is responsible
-/// for using this to establish communication with the isolate.
-///
-/// [packageRoot] controls the package root of the isolate. It may be either a
-/// [String] or a [Uri].
-///
-/// If [snapshot] is passed, the isolate will be loaded from that path if it
-/// exists. Otherwise, a snapshot of the isolate's code will be saved to that
-/// path once the isolate is loaded.
-Future runInIsolate(String code, message,
-    {packageRoot, String snapshot}) async {
-  if (snapshot != null && fileExists(snapshot)) {
-    log.fine("Spawning isolate from $snapshot.");
-    if (packageRoot != null) packageRoot = Uri.parse(packageRoot.toString());
-    try {
-      // Make the snapshot URI absolute to work around sdk#8440.
-      await Isolate.spawnUri(p.toUri(p.absolute(snapshot)), [], message,
-          packageRoot: packageRoot);
-      return;
-    } on IsolateSpawnException catch (error) {
-      log.fine("Couldn't load existing snapshot $snapshot:\n$error");
-      // Do nothing, we will regenerate the snapshot below.
-    }
-  }
-
-  await withTempDir((dir) async {
-    var dartPath = p.join(dir, 'runInIsolate.dart');
-    writeTextFile(dartPath, code, dontLogContents: true);
-    await Isolate.spawnUri(p.toUri(p.absolute(dartPath)), [], message,
-        packageRoot: packageRoot);
-
-    if (snapshot == null) return;
-
-    ensureDir(p.dirname(snapshot));
-    var snapshotArgs = <String>[];
-    if (packageRoot != null) snapshotArgs.add('--package-root=$packageRoot');
-    snapshotArgs.addAll(['--snapshot=$snapshot', dartPath]);
-    var result = await runProcess(Platform.executable, snapshotArgs);
-
-    if (result.success) return;
-
-    // Don't emit a fatal error here, since we don't want to crash the
-    // otherwise successful isolate load.
-    log.warning("Failed to compile a snapshot to "
-        "${p.relative(snapshot)}:\n" +
-        result.stderr.join("\n"));
-  });
 }
 
 /// Snapshots the Dart executable at [executableUrl] to a snapshot at
