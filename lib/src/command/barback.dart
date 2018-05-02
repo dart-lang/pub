ae06ffc18750c9ddea260377180cc1644ace58cd
Nate Bosch <nbosch@google.com>
2018-05-02 13:13:46 -0700

    Customize usage exception for build/serve
    Closes #1891
    
    It's not possible to customize the `argParser` in a `Command` such that
    it ignores these invalid options, instead customize the UsageException
    to have the same message as running the command.
    
    The difference now is that with arguments it will exit with code 64 and
    without arguments it will exit with code 1.

--- lib/src/command/barback.dart
+++ lib/src/command/barback.dart
@@ -2,12 +2,24 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import '../command.dart';
+import 'package:args/command_runner.dart';
+
 import '../log.dart' as log;
 import '../utils.dart';
 
 /// Shared base class for [BuildCommand] and [ServeCommand].
-abstract class BarbackCommand extends PubCommand {
+abstract class BarbackCommand extends Command {
+  @override
+  bool get takesArguments => false;
+
+  @override
+  usageException(_) => throw new UsageException(_deprecationError, '');
+
+  String get _deprecationError =>
+      log.red("Dart 2 has a new build system. Learn how to migrate "
+          "from ${log.bold('pub build')} and\n"
+          "${log.bold('pub serve')}: https://webdev.dartlang.org/dart-2\n");
+
   run() {
     // Switch to JSON output if specified. We need to do this before parsing
     // the source directories so an error will be correctly reported in JSON
@@ -15,8 +27,6 @@ abstract class BarbackCommand extends PubCommand {
     log.json.enabled =
         argResults.options.contains("format") && argResults["format"] == "json";
 
-    fail(log.red("Dart 2 has a new build system. Learn how to migrate "
-        "from ${log.bold('pub build')} and\n"
-        "${log.bold('pub serve')}: https://webdev.dartlang.org/dart-2\n"));
+    fail(_deprecationError);
   }
 }
