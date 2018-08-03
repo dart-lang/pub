99971955c9e820fe13a59ba462bdfe6de6e9daae
Nate Bosch <nbosch@google.com>
2018-08-03 11:47:19 -0700

    Restore nonexistent_pubspec_test

---
+++ test/upgrade/git/upgrade_to_nonexistent_pubspec_test.dart
@@ -0,0 +1,35 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:test/test.dart';
+
+import '../../descriptor.dart' as d;
+import '../../test_pub.dart';
+
+main() {
+  test("upgrades Git packages to a nonexistent pubspec", () async {
+    ensureGit();
+
+    var repo =
+        d.git('foo.git', [d.libDir('foo'), d.libPubspec('foo', '1.0.0')]);
+    await repo.create();
+
+    await d.appDir({
+      "foo": {"git": "../foo.git"}
+    }).create();
+
+    await pubGet();
+
+    final originalFooSpec = packageSpecLine('foo');
+
+    await repo.runGit(['rm', 'pubspec.yaml']);
+    await repo.runGit(['commit', '-m', 'delete']);
+
+    await pubUpgrade(
+        error: new RegExp(r'Could not find a file named "pubspec.yaml" '
+            r'in [^\n]*\.'));
+
+    expect(packageSpecLine('foo'), originalFooSpec);
+  });
+}
