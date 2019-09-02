82ca3594fe73c5344a38268adb14f0bebad36075
Jake Champion <me@jakechampion.name>
2019-09-02 17:12:18 +0100

    return the hashCode of the description

--- lib/src/source/hosted.dart
+++ lib/src/source/hosted.dart
@@ -95,7 +95,7 @@ class HostedSource extends Source {
   bool descriptionsEqual(description1, description2) =>
       _parseDescription(description1) == _parseDescription(description2);
 
-  int hashDescription(description) => _parseDescription.hashCode;
+  int hashDescription(description) => description.hashCode;
 
   /// Ensures that [description] is a valid hosted package description.
   ///
