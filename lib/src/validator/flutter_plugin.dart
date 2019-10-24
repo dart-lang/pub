c5db374a6f9868fcc17edb302b7be67c1d6a2cc7
Jonas Finnemann Jensen <jonasfj@google.com>
2019-10-24 13:33:10 +0200

    Updated warnings/error messages

--- lib/src/validator/flutter_plugin.dart
+++ lib/src/validator/flutter_plugin.dart
@@ -17,16 +17,18 @@ class FlutterPluginValidator extends Validator {
 
   Future validate() async {
     final pubspec = entrypoint.root.pubspec;
-    // Ignore all packages that do not have the `flutter.plugin` property.
+    // Ignore all packages that do not have the `flutter.plugin` property, or
+    // which does not have an SDK constraint on Flutter.
     if (pubspec.fields['flutter'] is! Map ||
-        pubspec.fields['flutter']['plugin'] is! Map) {
+        pubspec.fields['flutter']['plugin'] is! Map ||
+        pubspec.sdkConstraints['flutter'] == null) {
       return;
     }
     final plugin = pubspec.fields['flutter']['plugin'] as Map;
 
     // Determine if this uses the old format by checking if `flutter.plugin`
     // contains any of the following keys.
-    final usesOldPluginRegistrationFormat = {
+    final usesOldPluginFormat = {
       'androidPackage',
       'iosPrefix',
       'pluginClass',
@@ -34,32 +36,32 @@ class FlutterPluginValidator extends Validator {
 
     // Determine if this uses the new format by check if the:
     // `flutter.plugin.platforms` keys is defined.
-    final usesNewPluginRegistrationFormat = plugin['platforms'] != null;
+    final usesNewPluginFormat = plugin['platforms'] != null;
 
     // Report an error, if both the new and the old format is used.
-    if (usesOldPluginRegistrationFormat && usesNewPluginRegistrationFormat) {
-      errors.add('In pubspec.yaml the flutter.plugin.platforms plugin '
-          'registration format, cannot be used in combination with the old '
-          'flutter.plugin.{androidPackage,iosPrefix,pluginClass} '
-          'plugin registration format.');
+    if (usesOldPluginFormat && usesNewPluginFormat) {
+      errors.add('In pubspec.yaml the flutter.plugin.platforms key cannot be '
+          'used in combination with the old '
+          'flutter.plugin.{androidPackage,iosPrefix,pluginClass} keys.');
     }
 
     // If the new plugin format is used, and the flutter SDK dependency allows
-    // SDKs older than 1.12, then this is going to be a problem.
-    if (usesNewPluginRegistrationFormat &&
-        !pubspec.sdkConstraints['flutter'].allows(Version.parse('1.11'))) {
-      warnings.add('pubspec.yaml allows Flutter SDK version 1.11, which does '
-          'not support the flutter.plugin.platforms plugin registration '
-          'format. Please consider increasing the Flutter SDK requirement to '
-          '^1.12 (environment.sdk.flutter)');
+    // SDKs older than 1.10.0, then this is going to be a problem.
+    if (usesNewPluginFormat &&
+        pubspec.sdkConstraints['flutter'] != null &&
+        pubspec.sdkConstraints['flutter'].allows(Version.parse('1.9.0'))) {
+      errors.add('pubspec.yaml allows Flutter SDK version 1.9.0, which does '
+          'not support the flutter.plugin.platforms key.\n'
+          'Please consider increasing the Flutter SDK requirement to '
+          '^1.10.0 (environment.sdk.flutter)');
     }
 
-    // Warn if using the old plugin registration format.
-    if (usesOldPluginRegistrationFormat) {
-      warnings.add('The flutter.plugin.{androidPackage,iosPrefix,pluginClass} '
-          'plugin registration format is deprecated. Consider using the '
-          'flutter.plugin.platforms plugin registration format introduced in '
-          'Flutter 1.12');
+    // Warn if using the old plugin format.
+    if (usesOldPluginFormat) {
+      warnings.add('In pubspec.yaml the '
+          'flutter.plugin.{androidPackage,iosPrefix,pluginClass} keys are '
+          'deprecated. Consider using the flutter.plugin.platforms key '
+          'introduced in Flutter 1.10.0');
     }
   }
 }
