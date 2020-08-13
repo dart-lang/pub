8be0e44178d6b552b7245e935da8ed10fc4b0f79
Sigurd Meldgaard <sigurdm@google.com>
2020-08-13 12:37:03 +0200

    More^2 fix tool/test-bin/pub.bat

--- tool/test-bin/pub.bat
+++ tool/test-bin/pub.bat
@@ -5,7 +5,7 @@ rem BSD-style license that can be found in the LICENSE file.
 rem Runs the pub snapshot with dart from PATH (or the snapshot if present).
 
 if "%PUB_SNAPSHOT_FILE%"=="" (
-    dart %0\..\..\..\bin\pub.dart %*
+    dart %~p0\..\..\bin\pub.dart %*
 ) else (
-    dart %PUB_SNAPSHOT_FILE %*
+    dart %PUB_SNAPSHOT_FILE% %*
 )
