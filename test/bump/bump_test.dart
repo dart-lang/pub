60544ae85c096f063a1068ab3869751ae7b0315a
Garett Tok <garettok@hotmail.com>
2020-08-13 17:39:22 +0800

    Added error tests

--- test/bump/bump_test.dart
+++ test/bump/bump_test.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:pub/src/exit_codes.dart' as exit_codes;
 import 'package:test/test.dart';
 
 import '../descriptor.dart' as d;
@@ -22,6 +23,44 @@ void main() {
       d.pubspec({
         'name': 'myapp',
         'version': '1.2.4',
+      })
+    ]).validate();
+  });
+
+  test('bump to version', () async {
+    await d.dir(appPath, [
+      d.pubspec({
+        'name': 'myapp',
+        'version': '1.2.3',
+      })
+    ]).create();
+
+    await pubBump(args: ['4.5.6'], output: contains('1.2.3 to 4.5.6'));
+
+    await d.dir(appPath, [
+      d.pubspec({
+        'name': 'myapp',
+        'version': '4.5.6',
+      })
+    ]).validate();
+  });
+
+  test('bump to version with pre-release and build', () async {
+    await d.dir(appPath, [
+      d.pubspec({
+        'name': 'myapp',
+        'version': '1.2.3',
+      })
+    ]).create();
+
+    await pubBump(
+        args: ['4.5.6-alpha+build.1'],
+        output: contains('1.2.3 to 4.5.6-alpha+build.1'));
+
+    await d.dir(appPath, [
+      d.pubspec({
+        'name': 'myapp',
+        'version': '4.5.6-alpha+build.1',
       })
     ]).validate();
   });
@@ -132,5 +171,64 @@ void main() {
         'version': '1.2.4',
       })
     ]).validate();
+  });
+
+  group('fails if', () {
+    test('more than one flag is provided', () async {
+      await d.dir(appPath, [
+        d.pubspec({
+          'name': 'myapp',
+          'version': '1.2.3',
+        })
+      ]).create();
+
+      await pubBump(
+          args: ['--major', '--patch'],
+          error: contains('Only one flag should be specified at most'),
+          exitCode: exit_codes.USAGE);
+    });
+
+    test('a flag is provided along with an argument', () async {
+      await d.dir(appPath, [
+        d.pubspec({
+          'name': 'myapp',
+          'version': '1.2.3',
+        })
+      ]).create();
+
+      await pubBump(
+          args: ['--major', '4.5.6'],
+          error: contains(
+              'Must not specify a version to bump to along with flags'),
+          exitCode: exit_codes.USAGE);
+    });
+
+    test('more than one argument is provided', () async {
+      await d.dir(appPath, [
+        d.pubspec({
+          'name': 'myapp',
+          'version': '1.2.3',
+        })
+      ]).create();
+
+      await pubBump(
+          args: ['4.5.6', '4.5.7'],
+          error: contains('Please specify only one version to bump to'),
+          exitCode: exit_codes.USAGE);
+    });
+
+    test('invalid version to bump to is provided', () async {
+      await d.dir(appPath, [
+        d.pubspec({
+          'name': 'myapp',
+          'version': '1.2.3',
+        })
+      ]).create();
+
+      await pubBump(
+          args: ['a.b.c'],
+          error: contains('Invalid version a.b.c found.'),
+          exitCode: exit_codes.USAGE);
+    });
   });
 }
