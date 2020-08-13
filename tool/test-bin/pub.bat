720bff2cec4dcb14d4db1cc0f4656413d93bd64c
Sigurd Meldgaard <sigurdm@google.com>
2020-08-13 13:56:19 +0200

    More^3 fix tool/test-bin/pub.bat

--- tool/test-bin/pub.bat
+++ tool/test-bin/pub.bat
@@ -1,3 +1,4 @@
+@echo off
 rem Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
 rem for details. All rights reserved. Use of this source code is governed by a
 rem BSD-style license that can be found in the LICENSE file.
@@ -5,7 +6,7 @@ rem BSD-style license that can be found in the LICENSE file.
 rem Runs the pub snapshot with dart from PATH (or the snapshot if present).
 
 if "%PUB_SNAPSHOT_FILE%"=="" (
-    dart %~p0\..\..\bin\pub.dart %*
+    dart %~p0\..\..\..\bin\pub.dart %*
 ) else (
     dart %PUB_SNAPSHOT_FILE% %*
 )
