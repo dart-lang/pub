d224d4721d53295b02673e3f160cd7b246740542
Sigurd Meldgaard <sigurdm@google.com>
2020-11-09 11:31:11 +0100

    Remove it an anyways

--- lib/src/command_runner.dart
+++ lib/src/command_runner.dart
@@ -140,11 +140,6 @@ class PubCommandRunner extends CommandRunner<int> implements PubTopLevel {
   Future<int> runCommand(ArgResults topLevelResults) async {
     _checkDepsSynced();
 
-    if (Platform.environment['_PUB_DEPRECATED_COMMAND_WARNING'] == 'true') {
-      await log.warningsOnlyUnlessTerminal(() => log
-          .message('The `pub` command is deprecated. Run `dart pub` instead.'));
-    }
-
     if (topLevelResults['version']) {
       log.message('Pub ${sdk.version}');
       return 0;
