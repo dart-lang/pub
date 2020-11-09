889fca65c86b0edb30ae6004d423c4cfc5f8448b
Sigurd Meldgaard <sigurdm@google.com>
2020-11-09 10:26:39 +0100

    Revert "Remove deprecation"
    This reverts commit a1592625ab23c7534c1f8211f566edf4e10e1535.

--- lib/src/command_runner.dart
+++ lib/src/command_runner.dart
@@ -140,6 +140,9 @@ class PubCommandRunner extends CommandRunner<int> implements PubTopLevel {
   Future<int> runCommand(ArgResults topLevelResults) async {
     _checkDepsSynced();
 
+    await log.warningsOnlyUnlessTerminal(() => log
+        .message('The `pub` command is deprecated. Run `dart pub` instead.'));
+
     if (topLevelResults['version']) {
       log.message('Pub ${sdk.version}');
       return 0;
