e4e5c3ab84f133bd7b15d7fef5e8fc66158bc5cd
Sigurd Meldgaard <sigurdm@google.com>
2020-11-10 14:04:06 +0100

    Improve message in `pub outdated` when incompatible versions are found

--- lib/src/command/outdated.dart
+++ lib/src/command/outdated.dart
@@ -543,9 +543,8 @@ Future<void> _outputHuman(
       upgradable == 0 &&
       rows.isNotEmpty) {
     log.message(
-        '\nCurrent dependencies are constrained to versions listed in Resolvable.'
-        '\nA resolution compatible with the Latest versions doesn\'t exist, as they are not'
-        '\nmutually compatible.');
+        "You are already using the newest resolvable versions listed in the 'Resolvable' column.\n"
+        "Newer versions, listed in 'Latest', may not be mutually compatible.");
   }
 
   if (notAtResolvable != 0) {
