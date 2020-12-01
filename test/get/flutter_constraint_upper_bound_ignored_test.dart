9f0227a44c356a9002f09fd8d26b058df33492ea
Sigurd Meldgaard <sigurdm@google.com>
2020-12-01 14:57:21 +0100

    Deprecate flutter constraint upper bound

---
+++ test/get/flutter_constraint_upper_bound_ignored_test.dart
@@ -0,0 +1,28 @@
+// Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:test/test.dart';
+
+import 'package:pub/src/exit_codes.dart' as exit_codes;
+
+import '../descriptor.dart' as d;
+import '../test_pub.dart';
+
+void main() {
+  test('pub get succeeds despite of "invalid" flutter upper bound', () async {
+    final fakeFlutterRoot =
+        d.dir('fake_flutter_root', [d.file('version', '1.23.0')]);
+    await fakeFlutterRoot.create();
+    await d.dir(appPath, [
+      d.pubspec({
+        'name': 'myapp',
+        'environment': {'flutter': '>=0.5.0 <1.0.0'}
+      }),
+    ]).create();
+
+    await pubGet(
+        exitCode: exit_codes.SUCCESS,
+        environment: {'FLUTTER_ROOT': fakeFlutterRoot.io.path});
+  });
+}
