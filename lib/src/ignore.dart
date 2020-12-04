ef7431caed4327920b64060c2bfe051b462b849b
Sigurd Meldgaard <sigurdm@google.com>
2020-12-04 16:47:29 +0100

    Introduce ignore

---
+++ lib/src/ignore.dart
@@ -0,0 +1,356 @@
+// Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// An [Ignore] filter compatible with `.gitignore`.
+///
+/// An [Ignore] instance holds a set of [`.gitignore` rules][1], and allows
+/// testing if a given path is ignored.
+///
+/// **Example**:
+/// ```dart
+/// import 'package:ignore/ignore.dart';
+///
+/// void main() {
+///   final ignore = Ignore([
+///     '*.o',
+///   ]);
+///
+///   print(ignore.ignores('main.o')); // true
+///   print(ignore.ignores('main.c')); // false
+/// }
+/// ```
+///
+/// [1]: https://git-scm.com/docs/gitignore
+
+import 'package:meta/meta.dart';
+
+/// A set of ignore rules.
+///
+/// An [Ignore] instance holds a set of [`.gitignore` rules][1], and allows
+/// testing if a given path is ignored.
+///
+/// **Example**:
+/// ```dart
+/// import 'package:ignore/ignore.dart';
+///
+/// void main() {
+///   final ignore = Ignore([
+///     '*.o',
+///   ]);
+///
+///   print(ignore.ignores('main.o')); // true
+///   print(ignore.ignores('main.c')); // false
+/// }
+/// ```
+///
+/// [1]: https://git-scm.com/docs/gitignore
+@sealed
+class Ignore {
+  final List<GitIgnoreRule> _rules;
+
+  /// Create an [Ignore] instance with a set of [`.gitignore` compatible][1]
+  /// patterns.
+  ///
+  /// Each pattern in [patterns] will be interpreted as one or more lines from
+  /// a `.gitignore` file, in compliance with the [`.gitignore` manual page][1].
+  ///
+  /// If [ignoreCase] is `true`, patterns will be case-insensitive. By default
+  /// `git` is case-sensitive. But case insensitivity can be enabled when a
+  /// repository is created, or by configuration option, see
+  /// [`core.ignoreCase` documentation][2] for details.
+  ///
+  /// **Example**:
+  /// ```dart
+  /// import 'package:ignore/ignore.dart';
+  /// void main() {
+  ///   final ignore = Ignore([
+  ///     // You can pass an entire .gitignore file as a single string.
+  ///     // You can also pass it as a list of lines, or both.
+  ///     '''
+  /// # Comment in a .gitignore file
+  /// obj/
+  /// *.o
+  /// !main.o
+  ///   '''
+  ///   ]);
+  ///
+  ///   print(ignore.ignores('obj/README.md')); // true
+  ///   print(ignore.ignores('lib.o')); // false
+  ///   print(ignore.ignores('main.o')); // false
+  /// }
+  /// ```
+  ///
+  /// [1]: https://git-scm.com/docs/gitignore
+  /// [2]: https://git-scm.com/docs/git-config#Documentation/git-config.txt-coreignoreCase
+  Ignore(Iterable<String> patterns, {bool ignoreCase = false})
+      : _rules = parseIgnorePatterns(patterns, ignoreCase);
+
+  /// Returns `true` if [path] is ignored by the patterns used to create this
+  /// [Ignore] instance.
+  ///
+  /// The [path] must be a relative path, not starting with `./`, `../`, and
+  /// must end in slash (`/`) if it is directory.
+  ///
+  /// **Example**:
+  /// ```dart
+  /// import 'package:ignore/ignore.dart';
+  ///
+  /// void main() {
+  ///   final ignore = Ignore([
+  ///     '*.o',
+  ///   ]);
+  ///
+  ///   print(ignore.ignores('main.o')); // true
+  ///   print(ignore.ignores('main.c')); // false
+  ///   print(ignore.ignores('lib/')); // false
+  ///   print(ignore.ignores('lib/helper.o')); // true
+  ///   print(ignore.ignores('lib/helper.c')); // false
+  /// }
+  /// ```
+  bool ignores(String path) {
+    ArgumentError.checkNotNull(path, 'path');
+    if (path.isEmpty) {
+      throw ArgumentError.value(path, 'path', 'must be not empty');
+    }
+    if (path.startsWith('/') ||
+        path.startsWith('./') ||
+        path.startsWith('../') ||
+        path == '.' ||
+        path == '..') {
+      throw ArgumentError.value(
+        path,
+        'path',
+        'must be relative, and not start with "./", "../"',
+      );
+    }
+
+    // for (final p in _parentFolders(path).followedBy([path])) {
+    var ignored = false;
+    for (final r in _rules) {
+      if (ignored == r.negative && r.pattern.hasMatch(path)) {
+        ignored = !r.negative;
+      }
+    }
+    return ignored;
+  }
+
+  /// Removes entries from [paths] that are ignored the patterns used to create
+  /// this [Ignore] instance.
+  ///
+  /// The [path] must be a relative path, not starting with `./`, `../`, and
+  /// must end in slash (`/`) if it is directory.
+  ///
+  /// **Example**:
+  /// ```dart
+  /// import 'package:ignore/ignore.dart';
+  ///
+  /// void main() {
+  ///   final ignore = Ignore([
+  ///     '*.o',
+  ///   ]);
+  ///
+  ///   print(ignore.filter([
+  ///     'main.o',
+  ///     'main.c',
+  ///     'lib/',
+  ///     'lib/helper.o',
+  ///     'lib/helper.c',
+  ///   ])); // main.c lib/ helper.c
+  /// }
+  /// ```
+  ///
+  /// This method is semantically equivalent to `paths.where(ignore.ignores)`,
+  /// however, intermediate results for paths is reused which may improve
+  /// performance.
+  Iterable<String> filter(Iterable<String> paths) {
+    ArgumentError.checkNotNull(paths, 'paths');
+    // TODO: implement a cache
+    return paths.where((p) => !ignores(p));
+  }
+}
+
+class GitIgnoreRule {
+  final String original;
+  final RegExp pattern;
+  final bool negative;
+  GitIgnoreRule(this.original, this.pattern, this.negative);
+}
+
+List<GitIgnoreRule> parseIgnorePatterns(
+  Iterable<String> patterns,
+  bool ignoreCase,
+) {
+  ArgumentError.checkNotNull(patterns, 'patterns');
+  ArgumentError.checkNotNull(ignoreCase, 'ignoreCase');
+  if (patterns.contains(null)) {
+    throw ArgumentError.value(patterns, 'patterns', 'may not contain null');
+  }
+  return patterns
+      .map((s) => s.split('\n'))
+      .expand((e) => e)
+      .map((pattern) => parseIgnorePattern(pattern, ignoreCase))
+      .where((r) => r != null)
+      .toList();
+}
+
+GitIgnoreRule parseIgnorePattern(String pattern, bool ignoreCase) {
+  // Check if patterns is a comment
+  if (pattern.startsWith('#')) {
+    return null;
+  }
+  var first = 0;
+  var end = pattern.length;
+
+  // Detect negative patterns
+  final negative = pattern.startsWith('!');
+  if (negative) {
+    first++;
+  }
+  // Remove escape for # and !
+  if (pattern.startsWith(r'\#') || pattern.startsWith(r'\!')) {
+    first++;
+  }
+  // Remove trailing whitespace unless escaped
+  while (end != 0 &&
+      pattern[end - 1] == ' ' &&
+      (end == 1 || pattern[end - 2] != '\\')) {
+    end--;
+  }
+  // Empty patterns match nothing.
+  if (first == end) return null;
+
+  var current = first;
+  String peekChar() => current >= end ? null : pattern[current];
+
+  var expr = '';
+
+  // Parses the inside of a [] range. Returns the value as a RegExp character
+  // range, or null if the pattern was broken.
+  String parseCharacterRange() {
+    var characterRange = '';
+    var first = true;
+    for (;;) {
+      final nextChar = peekChar();
+      if (nextChar == null) {
+        print('a');
+        return null;
+      }
+      current++;
+      if (nextChar == '\\') {
+        final escaped = peekChar();
+        if (escaped == null) {
+          print('b');
+          return null;
+        }
+        current++;
+
+        characterRange += escaped == '-' ? r'\-' : RegExp.escape(escaped);
+      } else if (nextChar == '!' && first) {
+        characterRange += '^';
+      } else if (nextChar == ']' && first) {
+        characterRange += RegExp.escape(nextChar);
+      } else if (nextChar == ']') {
+        assert(!first);
+        return characterRange;
+      } else {
+        characterRange += nextChar;
+      }
+      first = false;
+    }
+  }
+
+  var relativeToPath = false;
+  var matchesDirectoriesOnly = false;
+
+  // slashes have different significance depending on where they are in
+  // the String. Handle that here.
+  // Returns the regexp to match the just parsed slash.
+  void handleSlash() {
+    if (current == end) {
+      // A slash at the end makes us only match directories.
+      matchesDirectoriesOnly = true;
+    } else {
+      // A slash anywhere else makes the pattern relative anchored at the
+      // current path.
+      relativeToPath = true;
+    }
+  }
+
+  for (;;) {
+    final nextChar = peekChar();
+    if (nextChar == null) break;
+    current++;
+    if (nextChar == '*') {
+      if (peekChar() == '*') {
+        // Handle '**'
+        current++;
+        if (peekChar() == '/') {
+          current++;
+          if (current == end) {
+            expr += '.*/';
+          } else {
+            // Match nothing or a path followed by '/'
+            expr += '(?:(?:)|(?:.*/))';
+          }
+          // Handle the side effects of seeing a slash.
+          // We know it was not initial, so the return value is ignored.
+          handleSlash();
+        } else {
+          expr += '.*';
+        }
+      } else {
+        // Handle a single '*'
+        expr += '[^/]*';
+      }
+    } else if (nextChar == '?') {
+      // Handle '?'
+      expr += '[^/]';
+    } else if (nextChar == '[') {
+      // Character ranges
+      final characterRange = parseCharacterRange();
+      if (characterRange == null) {
+        print('c');
+        return null;
+      }
+      expr += '[$characterRange]';
+    } else if (nextChar == '\\') {
+      // Escapes
+      final escaped = peekChar();
+      if (escaped == null) {
+        print('d');
+        return null;
+      }
+      expr += RegExp.escape(escaped);
+      current++;
+    } else {
+      if (nextChar == '/') {
+        if (current - 1 != first) {
+          // If slash appears in the beginning we don't want it manifest in the
+          // regexp.
+          expr += '/';
+        }
+        handleSlash();
+      } else {
+        expr += RegExp.escape(nextChar);
+      }
+    }
+  }
+  if (relativeToPath) {
+    expr = '^$expr';
+  } else {
+    expr = '(?:^|/)$expr';
+  }
+  if (!matchesDirectoriesOnly) {
+    expr = '$expr(?:\$|/)';
+  }
+  try {
+    print('pattern "$pattern" = "$expr"');
+    return GitIgnoreRule(
+        pattern, RegExp(expr, caseSensitive: ignoreCase), negative);
+  } on FormatException catch (e) {
+    // TODO: Print a nicer error
+    throw AssertionError(
+        'Created broken expression "$expr" from ignore pattern "$pattern" -> $e');
+  }
+}
---
+++ test/ignore_test.dart
@@ -0,0 +1,707 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import 'dart:collection';
+import 'dart:io';
+
+import 'package:test/test.dart';
+import 'package:pub/src/ignore.dart';
+
+void main() {
+  group('pub', () {
+    for (final c in testData) {
+      c.paths.forEach(
+        (path, expected) => test(
+          '${c.name}: Ignore.ignores("$path") == $expected',
+          () {
+            final ignore = Ignore(c.patterns);
+            if (expected != ignore.ignores(path)) {
+              if (expected) {
+                fail('Expected "$path" to be ignored, it was NOT!');
+              }
+              fail('Expected "$path" to NOT be ignored, it was IGNORED!');
+            }
+          },
+        ),
+      );
+    }
+  });
+
+  group('git', () {
+    Directory tmp;
+    setUpAll(() async {
+      tmp = await Directory.systemTemp.createTemp('package-ignore-test-');
+      final ret = await Process.run(
+        'git',
+        ['init'],
+        includeParentEnvironment: false,
+        workingDirectory: tmp.path,
+      );
+      expect(ret.exitCode, equals(0), reason: 'Running "git init" failed');
+    });
+    tearDownAll(() async {
+      await tmp.delete(recursive: true);
+      tmp = null;
+    });
+    for (final c in testData) {
+      c.paths.forEach(
+        (path, expected) => test(
+          '${c.name}: git check-ignore "$path" is ${expected ? 'IGNORED' : 'NOT ignored'}',
+          () async {
+            final gitIgnore = File.fromUri(tmp.uri.resolve('.gitignore'));
+            await gitIgnore.writeAsString(c.patterns.join('\n') + '\n');
+            final ret = await Process.run(
+              'git',
+              ['check-ignore', '--no-index', path],
+              includeParentEnvironment: false,
+              workingDirectory: tmp.path,
+            );
+            expect(
+              ret.exitCode,
+              anyOf(0, 1),
+              reason: 'Running "git check-ignore" failed',
+            );
+            final ignored = ret.exitCode == 0;
+            if (expected != ignored) {
+              if (expected) {
+                fail('Expected "$path" to be ignored, it was NOT!');
+              }
+              fail('Expected "$path" to NOT be ignored, it was IGNORED!');
+            }
+          },
+        ),
+      );
+    }
+  });
+}
+
+class TestData {
+  /// Name of the test case.
+  final String name;
+
+  /// Patterns for the test case.
+  final List<String> patterns;
+
+  /// Map from path to `true` if ignored by [patterns], and `false` if not
+  /// ignored by `patterns`.
+  final Map<String, bool> paths;
+  TestData(this.name, Iterable<String> patterns, Map<String, bool> paths)
+      : patterns = UnmodifiableListView(List.from(patterns)),
+        paths = UnmodifiableMapView(Map.from(paths));
+  TestData.single(String pattern, Map<String, bool> paths)
+      : name = '"${pattern.replaceAll('\n', '\\n')}"',
+        patterns = UnmodifiableListView([pattern]),
+        paths = UnmodifiableMapView(Map.from(paths));
+}
+
+final testData = [
+  // Simple test case
+  TestData('simple', [
+    '/.git/',
+    '*.o',
+  ], {
+    '.git/config': true,
+    '.git/': true,
+    'README.md': false,
+    'main.c': false,
+    'main.o': true,
+  }),
+  // Test empty lines
+  TestData('empty', [
+    ''
+  ], {
+    'README.md': false,
+  }),
+  // Test simple patterns
+  TestData.single('file.txt', {
+    'file.txt': true,
+    'other.txt': false,
+    'src/file.txt': true,
+    '.obj/file.txt': true,
+    'sub/folder/file.txt': true,
+  }),
+  TestData.single('/file.txt', {
+    'file.txt': true,
+    'other.txt': false,
+    'src/file.txt': false,
+    '.obj/file.txt': false,
+    'sub/folder/file.txt': false,
+  }),
+  // Test comments and escaping
+  TestData.single('#file.txt', {
+    'file.txt': false,
+    '#file.txt': false,
+  }),
+  TestData.single(r'\#file.txt', {
+    '#file.txt': true,
+    'other.txt': false,
+    'src/#file.txt': true,
+    '.obj/#file.txt': true,
+    'sub/folder/#file.txt': true,
+  }),
+  // Test ! and escaping
+  TestData.single('!file.txt', {
+    'file.txt': false,
+    '!file.txt': false,
+  }),
+  TestData.single(r'\!file.txt', {
+    '!file.txt': true,
+    'other.txt': false,
+    'src/!file.txt': true,
+    '.obj/!file.txt': true,
+    'sub/folder/!file.txt': true,
+  }),
+  // Test trailing spaces and escaping
+  TestData.single('file.txt   ', {
+    'file.txt': true,
+    'other.txt': false,
+    'src/file.txt': true,
+    '.obj/file.txt': true,
+    'sub/folder/file.txt': true,
+  }),
+  TestData.single(r'file.txt\ \     ', {
+    'file.txt  ': true,
+    'file.txt': false,
+    'other.txt  ': false,
+    'src/file.txt  ': true,
+    'src/file.txt': false,
+    '.obj/file.txt  ': true,
+    '.obj/file.txt': false,
+    'sub/folder/file.txt  ': true,
+    'sub/folder/file.txt': false,
+  }),
+  // Test ending in a slash or not
+  TestData.single('folder/', {
+    'file.txt': false,
+    'folder': false,
+    'folder/': true,
+    'folder/file.txt': true,
+    'sub/folder/': true,
+    'sub/folder': false,
+    'sub/file.txt': false,
+  }),
+  TestData.single('folder.txt/', {
+    'file.txt': false,
+    'folder.txt': false,
+    'folder.txt/': true,
+    'folder.txt/file.txt': true,
+    'sub/folder.txt/': true,
+    'sub/folder.txt': false,
+    'sub/file.txt': false,
+  }),
+  TestData.single('folder', {
+    'file.txt': false,
+    'folder': true,
+    'folder/': true,
+    'folder/file.txt': true,
+    'sub/folder/': true,
+    'sub/folder': true,
+    'sub/file.txt': false,
+  }),
+  TestData.single('folder.txt', {
+    'file.txt': false,
+    'folder.txt': true,
+    'folder.txt/': true,
+    'folder.txt/file.txt': true,
+    'sub/folder.txt/': true,
+    'sub/folder.txt': true,
+    'sub/file.txt': false,
+  }),
+  // Test contains a slash makes it relative root
+  TestData.single('/folder/', {
+    'file.txt': false,
+    'folder': false,
+    'folder/': true,
+    'folder/file.txt': true,
+    'sub/folder/': false,
+    'sub/folder': false,
+    'sub/file.txt': false,
+  }),
+  TestData.single('/folder', {
+    'file.txt': false,
+    'folder': true,
+    'folder/': true,
+    'folder/file.txt': true,
+    'sub/folder/': false,
+    'sub/folder': false,
+    'sub/file.txt': false,
+  }),
+  TestData.single('sub/folder/', {
+    'file.txt': false,
+    'folder': false,
+    'folder/': false,
+    'folder/file.txt': false,
+    'sub/folder/': true,
+    'sub/folder/file.txt': true,
+    'sub/folder': false,
+    'sub/file.txt': false,
+  }),
+  TestData.single('sub/folder', {
+    'file.txt': false,
+    'folder': false,
+    'folder/': false,
+    'folder/file.txt': false,
+    'sub/folder/': true,
+    'sub/folder/file.txt': true,
+    'sub/folder': true,
+    'sub/file.txt': false,
+  }),
+  // Special characters from RegExp that are not special in .gitignore
+  for (final c in r'(){}+.^$|'.split('')) ...[
+    TestData.single('${c}file.txt', {
+      '${c}file.txt': true,
+      'file.txt': false,
+      'file.txt${c}': false,
+    }),
+    TestData.single('file.txt${c}', {
+      'file.txt${c}': true,
+      'file.txt': false,
+      '${c}file.txt': false,
+    }),
+    TestData.single('fi${c}l)e.txt', {
+      'fi${c}l)e.txt': true,
+      'f${c}il)e.txt': false,
+      'fil)e.txt': false,
+    }),
+    TestData.single('fi${c}l}e.txt', {
+      'fi${c}l}e.txt': true,
+      'f${c}il}e.txt': false,
+      'fil}e.txt': false,
+    }),
+  ],
+  // Special characters from RegExp that are also special in .gitignore
+  // can be escaped.
+  for (final c in r'[]*?\'.split('')) ...[
+    TestData.single('\\${c}file.txt', {
+      '${c}file.txt': true,
+      'file.txt': false,
+      'file.txt${c}': false,
+    }),
+    TestData.single('file.txt\\${c}', {
+      'file.txt${c}': true,
+      'file.txt': false,
+      '${c}file.txt': false,
+    }),
+    TestData.single('fi\\${c}l)e.txt', {
+      'fi${c}l)e.txt': true,
+      'f${c}il)e.txt': false,
+      'fil)e.txt': false,
+    }),
+    TestData.single('fi\\${c}l}e.txt', {
+      'fi${c}l}e.txt': true,
+      'f${c}il}e.txt': false,
+      'fil}e.txt': false,
+    }),
+  ],
+  // Special characters from RegExp can always be escaped
+  for (final c in r'()[]{}*+?.^$|\'.split('')) ...[
+    TestData.single('\\${c}file.txt', {
+      '${c}file.txt': true,
+      'file.txt': false,
+      'file.txt${c}': false,
+    }),
+    TestData.single('file.txt\\${c}', {
+      'file.txt${c}': true,
+      'file.txt': false,
+      '${c}file.txt': false,
+    }),
+    TestData.single('file\\${c}.txt', {
+      'file${c}.txt': true,
+      'file.txt': false,
+      '${c}file.txt': false,
+    }),
+  ],
+  // Ending in backslash (unescaped)
+  TestData.single('file.txt\\', {
+    'file.txt\\': false,
+    'file.txt ': false,
+    'file.txt\n': false,
+    'file.txt': false,
+  }),
+  TestData.single('file.txt\\\n', {
+    'file.txt\\\n': false,
+    'file.txt ': false,
+    'file.txt\n': false,
+    'file.txt': false,
+  }),
+  TestData.single('**\\', {
+    'file.txt\\\n': false,
+    'file.txt ': false,
+    'file.txt\n': false,
+    'file.txt': false,
+  }),
+  TestData.single('*\\', {
+    'file.txt\\\n': false,
+    'file.txt ': false,
+    'file.txt\n': false,
+    'file.txt': false,
+  }),
+  // ? matches anything except /
+  TestData.single('?', {
+    'f': true,
+    'file.txt': false,
+  }),
+  TestData.single('a?c', {
+    'abc': true,
+    'abcd': false,
+    'a/b': false,
+    'ab/': false,
+    'folder': false,
+    'folder/': false,
+    'folder/abc': true,
+    'folder/abcd': false,
+    'folder/aac': true,
+    'abc/': true,
+    'abc/file.txt': true,
+  }),
+  TestData.single('???', {
+    'abc': true,
+    'abcd': false,
+    'a/b': false,
+    'ab/': false,
+    'folder': false,
+    'folder/': false,
+    'folder/abc': true,
+    'folder/abcd': false,
+    'folder/aaa': true,
+    'abc/': true,
+    'abc/file.txt': true,
+  }),
+  TestData.single('/???', {
+    'abc': true,
+    'abcd': false,
+    'a/b': false,
+    'ab/': false,
+    'folder': false,
+    'folder/': false,
+    'folder/abc': false,
+    'folder/abcd': false,
+    'folder/aaa': false,
+    'abc/': true,
+    'abc/file.txt': true,
+  }),
+  TestData.single('???/', {
+    'abc': false,
+    'abcd': false,
+    'a/b': false,
+    'ab/': false,
+    'folder': false,
+    'folder/': false,
+    'folder/abc': false,
+    'folder/abcd': false,
+    'folder/aaa': false,
+    'abc/': true,
+    'abc/file.txt': true,
+  }),
+  TestData.single('???/file.txt', {
+    'abc': false,
+    'folder': false,
+    'folder/': false,
+    'folder/abc': false,
+    'folder/abcd': false,
+    'folder/aaa': false,
+    'abc/': false,
+    'abc/file.txt': true,
+  }),
+  // Empty character classes
+  TestData.single('a[]c', {
+    'abc': false,
+    'ac': false,
+    'abc': false,
+    'a': false,
+    'a[]c': false,
+    'c': false,
+  }),
+  TestData.single('a[]', {
+    'abc': false,
+    'ac': false,
+    'abc': false,
+    'a': false,
+    'a[]': false,
+    'c': false,
+  }),
+  // Invalid character classes
+  TestData.single(r'a[\]', {
+    'abc': false,
+    'ac': false,
+    'abc': false,
+    'a': false,
+    'a\\': false,
+    'a[]': false,
+    'a[': false,
+    'a[\\]': false,
+    'c': false,
+  }),
+  TestData.single(r'a[\\\]', {
+    'abc': false,
+    'ac': false,
+    'abc': false,
+    'a': false,
+    'a[]': false,
+    'a[': false,
+    'a[\\]': false,
+    'c': false,
+  }),
+  // Character classes with special characters
+  TestData.single(r'a[\\]', {
+    'a': false,
+    'ab': false,
+    'a[]': false,
+    'a[': false,
+    'a\\': true,
+  }),
+  TestData.single(r'a[^b]', {
+    'a': false,
+    'ab': false,
+    'ac': true,
+    'a[': true,
+    'a\\': true,
+  }),
+  TestData.single(r'a[!b]', {
+    'a': false,
+    'ab': false,
+    'ac': true,
+    'a[': true,
+    'a\\': true,
+  }),
+  TestData.single(r'a[[]', {
+    'a': false,
+    'ab': false,
+    'a[': true,
+    'a]': false,
+  }),
+  TestData.single(r'a[]]', {
+    'a': false,
+    'ab': false,
+    'a[': false,
+    'a]': true,
+  }),
+  TestData.single(r'a[?]', {
+    'a': false,
+    'ab': false,
+    'a??': false,
+    'a?': true,
+  }),
+  // Character classes with characters
+  TestData.single(r'a[abc]', {
+    'a': false,
+    'aa': true,
+    'ab': true,
+    'ac': true,
+    'ad': false,
+  }),
+  // Character classes with ranges
+  TestData.single(r'a[a-c]', {
+    'a': false,
+    'aa': true,
+    'ab': true,
+    'ac': true,
+    'ad': false,
+    'ae': false,
+  }),
+  TestData.single(r'a[a-cf]', {
+    'a': false,
+    'aa': true,
+    'ab': true,
+    'ac': true,
+    'ad': false,
+    'ae': false,
+    'af': true,
+  }),
+  TestData.single(r'a[a-cx-z]', {
+    'a': false,
+    'aa': true,
+    'ab': true,
+    'ac': true,
+    'ad': false,
+    'ae': false,
+    'af': false,
+    'ax': true,
+    'ay': true,
+    'az': true,
+  }),
+  // Character classes with weird-ranges
+  TestData.single(r'a[a-c-e]', {
+    'a': false,
+    'aa': true,
+    'ab': true,
+    'ac': true,
+    'ad': false,
+    'af': false,
+    'ae': true,
+    'a-': true,
+  }),
+  TestData.single(r'a[--0]', {
+    'a': false,
+    'a-': true,
+    'a.': true,
+    'a0': true,
+    'a1': false,
+  }),
+  TestData.single(r'a[+--]', {
+    'a': false,
+    'a-': true,
+    'a+': true,
+    'a,': true,
+    'a0': false,
+  }),
+  TestData.single(r'a[a-c]', {
+    'a': false,
+    'aa': true,
+    'ab': true,
+    'ac': true,
+    'ad': false,
+    'a-': false,
+  }),
+  TestData.single(r'a[\a-c]', {
+    'a': false,
+    'a\\': false,
+    'aa': true,
+    'ab': true,
+    'ac': true,
+    'ad': false,
+    'a-': false,
+  }),
+  TestData.single(r'a[a-\c]', {
+    'a': false,
+    'a\\': false,
+    'aa': true,
+    'ab': true,
+    'ac': true,
+    'ad': false,
+    'a-': false,
+  }),
+  TestData.single(r'a[\a-\c]', {
+    'a': false,
+    'a\\': false,
+    'aa': true,
+    'ab': true,
+    'ac': true,
+    'ad': false,
+    'a-': false,
+  }),
+  TestData.single(r'a[\a\-\c]', {
+    'a': false,
+    'a\\': false,
+    'aa': true,
+    'ab': false,
+    'a-': true,
+    'ac': true,
+    'ad': false,
+  }),
+  // Character classes with dashes
+  TestData.single(r'a[-]', {
+    'a-': true,
+    'a': false,
+  }),
+  TestData.single(r'a[a-]', {
+    'a-': true,
+    'aa': true,
+    'ab': false,
+  }),
+  TestData.single(r'a[-a]', {
+    'a-': true,
+    'aa': true,
+    'ab': false,
+  }),
+  // TODO: test slashes in character classes
+  // Test **, *, [, and [...] cases
+  TestData.single('x[a-c-e]', {
+    'xa': true,
+    'xb': true,
+    'xc': true,
+    'cd': false,
+    'xe': true,
+    'x-': true,
+  }),
+  TestData.single('*', {
+    'file.txt': true,
+    'other.txt': true,
+    'src/file.txt': true,
+    '.obj/file.txt': true,
+    'sub/folder/file.txt': true,
+  }),
+  TestData.single('f*', {
+    'file.txt': true,
+    'otherf.txt': false,
+    'src/file.txt': true,
+    'folder/other.txt': true,
+    'sub/folder/file.txt': true,
+  }),
+  TestData.single('*f', {
+    'file.txt': false,
+    'otherf.txt': false,
+    'otherf.paf': true,
+    'src/file.txt': false,
+    'folder/other.txt': false,
+    'sub/folderf/file.txt': true,
+  }),
+  TestData.single('sub/**/f*', {
+    'file.txt': false,
+    'otherf.txt': false,
+    'other.paf': false,
+    'src/file.txt': false,
+    'folder/other.txt': false,
+    'sub/file.txt': true,
+    'sub/f.txt': true,
+    'sub/pile.txt': false,
+    'sub/other.paf': false,
+    'sub/folder/file.txt': true,
+    'sub/folder/': true,
+    'sub/folder/pile.txt': true,
+    'sub/folder/other.paf': true,
+    'sub/bolder/': false,
+    'sub/bolder/file.txt': true,
+    'sub/bolder/pile.txt': false,
+    'sub/bolder/other.paf': false,
+    'subblob/file.txt': false,
+  }),
+  TestData.single('sub/**/', {
+    'file.txt': false,
+    'otherf.txt': false,
+    'other.paf': false,
+    'src/file.txt': false,
+    'folder/other.txt': false,
+    'sub/file.txt': false,
+    'sub/f.txt': false,
+    'sub/pile.txt': false,
+    'sub/other.paf': false,
+    'sub/folder/': true,
+    'sub/sub/folder/': true,
+    'sub/folder/file.txt': true,
+    'sub/folder/pile.txt': true,
+    'sub/folder/other.paf': true,
+    'sub/bolder/': true,
+    'sub/': false,
+    'sub/bolder/file.txt': true,
+    'sub/bolder/pile.txt': true,
+    'sub/bolder/other.paf': true,
+    'subblob/file.txt': false,
+  }),
+  TestData.single('**/bolder/', {
+    'file.txt': false,
+    'otherf.txt': false,
+    'other.paf': false,
+    'src/file.txt': false,
+    'sub/folder/bolder': false,
+    'sub/folder/other.paf': false,
+    'sub/bolder/': true,
+    'sub/': false,
+    'bolder/': true,
+    'bolder': false,
+    'sub/bolder/file.txt': true,
+    'sub/bolder/pile.txt': true,
+    'sub/bolder/other.paf': true,
+    'subblob/file.txt': false,
+  }),
+];
