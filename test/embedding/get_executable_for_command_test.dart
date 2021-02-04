9c2686d82894087cf44618da5019c549126f1a02
Sigurd Meldgaard <sigurdm@google.com>
2021-02-04 15:29:53 +0100

    Simplify expectabtion

--- test/embedding/get_executable_for_command_test.dart
+++ test/embedding/get_executable_for_command_test.dart
@@ -88,16 +88,12 @@ Future<void> main() async {
       }),
     ]).create();
     final dir = d.path(appPath);
-
-    final top = Platform.isWindows ? ',' : '╷';
-    final middle = Platform.isWindows ? r'|' : '│';
-    final bottom = Platform.isWindows ? r'\' : '╵';
-    await testGetExecutable('foo:app', dir, errorMessage: '''
-Error on line 1, column 9 of ${d.sandbox}${p.separator}foo${p.separator}pubspec.yaml: "name" field must be a valid Dart identifier.
-  $top
-1 $middle {"name":"broken name","environment":{"sdk":">=0.1.2 <1.0.0"}}
-  $middle         ^^^^^^^^^^^^^
-  $bottom''');
+    await testGetExecutable('foo:app', dir,
+        errorMessage: allOf(
+            contains(
+                'Error on line 1, column 9 of ${d.sandbox}${p.separator}foo${p.separator}pubspec.yaml: "name" field must be a valid Dart identifier.'),
+            contains(
+                '{"name":"broken name","environment":{"sdk":">=0.1.2 <1.0.0"}}')));
   });
 
   test('Does `pub get` if there is a pubspec.yaml', () async {
