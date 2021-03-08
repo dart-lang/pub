293d92a7eea0ccad0cbdfcab772beb297336d6aa
Sigurd Meldgaard <sigurdm@google.com>
2021-03-08 11:08:22 +0100

    Remove unused import

--- test/deps/executables_test.dart
+++ test/deps/executables_test.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:path/path.dart' as p;
 import 'package:pub/src/ascii_tree.dart' as tree;
 import 'package:pub/src/io.dart';
 import 'package:test/test.dart';
