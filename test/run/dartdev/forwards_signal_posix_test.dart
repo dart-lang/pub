2cf7e0377fa0fe6d0e0ab7a8c419bf58ea3f0409
Jonas Finnemann Jensen <jonasfj@google.com>
2021-05-06 16:33:37 +0200

    Being gradual migration to null-safety

--- test/run/dartdev/forwards_signal_posix_test.dart
+++ test/run/dartdev/forwards_signal_posix_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// @dart=2.10
+
 // Windows doesn't support sending signals.
 @TestOn('!windows')
 import 'dart:io';
