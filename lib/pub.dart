291705ca0b9632cb945dd39493dd5b9db41b897a
Jonas Finnemann Jensen <jonasfj@google.com>
2021-05-07 17:39:33 +0200

    Being gradual migration to null-safety (#2988)

--- lib/pub.dart
+++ lib/pub.dart
@@ -1,3 +1,9 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// @dart=2.10
+
 import 'package:args/command_runner.dart';
 import 'src/pub_embeddable_command.dart';
 export 'src/executable.dart'
--- lib/src/io.dart
+++ lib/src/io.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// @dart=2.10
+
 /// Helper functionality to make working with IO easier.
 import 'dart:async';
 import 'dart:collection';
