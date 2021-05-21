b2194dee6ecb9fc9873ad28271ed6ef79b6a0943
Misir Jafarov <misir.ceferov@gmail.com>
2021-05-22 02:38:23 +0400

    Added file headers

--- lib/src/authentication/client.dart
+++ lib/src/authentication/client.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 // ignore_for_file: import_of_legacy_library_into_null_safe
 
 import 'package:http/http.dart' as http;
