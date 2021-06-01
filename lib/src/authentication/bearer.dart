4ee285a92e6d52968d8327ff0016e17f5955e9b0
Misir Jafarov <misir.ceferov@gmail.com>
2021-06-01 11:39:12 +0400

    Annotate BearerCredential with @sealed

--- lib/src/authentication/bearer.dart
+++ lib/src/authentication/bearer.dart
@@ -2,10 +2,13 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:meta/meta.dart';
+
 import 'credential.dart';
 
 /// Bearer credential type that simply puts authorization header formatted as
 /// `Bearer $token` to request.header.
+@sealed
 class BearerCredential extends Credential {
   BearerCredential(this.token);
 
