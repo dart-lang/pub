93a3884494536bfb87f4d13c2c609707d0d59aa6
Sigurd Meldgaard <sigurdm@google.com>
2021-08-31 14:08:37 +0200

    Dart format

--- lib/src/command/add.dart
+++ lib/src/command/add.dart
@@ -69,7 +69,8 @@ class AddCommand extends PubCommand {
     argParser.addOption('sdk', help: 'SDK source for package');
     argParser.addFlag(
       'example',
-      help: 'Also update dependencies in `example/` after modifying pubspec.yaml in the root package (if it exists).',
+      help:
+          'Also update dependencies in `example/` after modifying pubspec.yaml in the root package (if it exists).',
       hide: true,
     );
 
