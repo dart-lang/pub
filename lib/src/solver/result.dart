0fd34ad8a08d95ee733b8efddc2f241ec57321b2
Sigurd Meldgaard <sigurdm@google.com>
2021-10-15 21:06:55 +0000

    Allow the analytics instance to be created later

--- lib/src/solver/result.dart
+++ lib/src/solver/result.dart
@@ -133,8 +133,10 @@ class SolveResult {
   }
 
   /// Send analytics about the package resolution.
-  void sendAnalytics(PubAnalytics analytics) {
-    ArgumentError.checkNotNull(analytics);
+  void sendAnalytics(PubAnalytics pubAnalytics) {
+    ArgumentError.checkNotNull(pubAnalytics);
+    final analytics = pubAnalytics.analytics;
+    if (analytics == null) return;
 
     for (final package in packages) {
       final source = package.source;
@@ -147,21 +149,21 @@ class SolveResult {
           DependencyType.direct: 'direct',
           DependencyType.none: 'transitive'
         }[_root.dependencyType(package.name)];
-        analytics.analytics.sendEvent(
+        analytics.sendEvent(
           'pub-get',
           package.name,
           label: package.version.canonicalizedVersion,
           value: 1,
           parameters: {
             'ni': '1', // We consider a pub-get a non-interactive event.
-            analytics.dependencyKindCustomDimensionName: dependencyKind,
+            pubAnalytics.dependencyKindCustomDimensionName: dependencyKind,
           },
         );
         log.fine(
             'Sending analytics hit for "pub-get" of ${package.name} version ${package.version} as dependency-kind $dependencyKind');
       }
     }
-    analytics.analytics.sendTiming(
+    analytics.sendTiming(
       'resolution',
       resolutionTime.inMilliseconds,
       category: 'pub-get',
