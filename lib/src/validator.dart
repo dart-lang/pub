d5881f8fb2d793ece78ecd61273c6c14a5dfd45e
Sarah Zakarias <zarah@google.com>
2021-10-15 15:39:58 +0200

    format

--- lib/src/validator.dart
+++ lib/src/validator.dart
@@ -72,7 +72,8 @@ abstract class Validator {
   void validateSdkConstraint(Version firstSdkVersion, String message) {
     // If the SDK constraint disallowed all versions before [firstSdkVersion],
     // no error is necessary.
-    if (entrypoint.root.pubspec.originalDartSdkConstraint.intersect(VersionRange(max: firstSdkVersion))
+    if (entrypoint.root.pubspec.originalDartSdkConstraint
+        .intersect(VersionRange(max: firstSdkVersion))
         .isEmpty) {
       return;
     }
@@ -92,7 +93,8 @@ abstract class Validator {
             ? firstSdkVersion.nextPatch
             : firstSdkVersion.nextBreaking);
 
-    var newSdkConstraint = entrypoint.root.pubspec.originalDartSdkConstraint.intersect(allowedSdks);
+    var newSdkConstraint = entrypoint.root.pubspec.originalDartSdkConstraint
+        .intersect(allowedSdks);
     if (newSdkConstraint.isEmpty) newSdkConstraint = allowedSdks;
 
     errors.add('$message\n'
