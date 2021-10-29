39807da43b5ce7a9c5f331b5243bf0fcf63fab8d
Sarah Zakarias <zarah@google.com>
2021-10-29 14:09:29 +0200

    migrate test/global/ to null-safety

--- test/global/binstubs/binstub_runs_executable_test.dart
+++ test/global/binstubs/binstub_runs_executable_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart=2.10
-
 import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
 import 'package:test_process/test_process.dart';
