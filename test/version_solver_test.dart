3275f0ec978b24f0e1f284ed5ff2ff24f0123dc1
Gabriel Terwesten <gabriel@terwesten.net>
2022-02-21 16:56:28 +0100

    Add version solver tests

--- test/version_solver_test.dart
+++ test/version_solver_test.dart
@@ -2012,6 +2012,46 @@ void override() {
 
     await expectResolves(result: {'foo': '1.2.3', 'bar': '0.0.1'});
   });
+
+  test('overrides in pubspec_overrides.yaml', () async {
+    await servePackages((builder) {
+      builder.serve('a', '1.0.0');
+      builder.serve('a', '2.0.0');
+    });
+
+    await d.dir(appPath, [
+      d.pubspec({
+        'name': 'myapp',
+        'dependencies': {'a': '1.0.0'},
+      }),
+      d.pubspecOverrides({
+        'dependency_overrides': {'a': '2.0.0'}
+      }),
+    ]).create();
+
+    await expectResolves(result: {'a': '2.0.0'});
+  });
+
+  test('pubspec_overrides.yaml takes precedence over pubspec.yaml', () async {
+    await servePackages((builder) {
+      builder.serve('a', '1.0.0');
+      builder.serve('a', '2.0.0');
+      builder.serve('a', '3.0.0');
+    });
+
+    await d.dir(appPath, [
+      d.pubspec({
+        'name': 'myapp',
+        'dependencies': {'a': '1.0.0'},
+        'dependency_overrides': {'a': '2.0.0'}
+      }),
+      d.pubspecOverrides({
+        'dependency_overrides': {'a': '3.0.0'}
+      }),
+    ]).create();
+
+    await expectResolves(result: {'a': '3.0.0'});
+  });
 }
 
 void downgrade() {
