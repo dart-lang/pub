64d7218423254610fcfe2beec7d52c0ab227ed07
Sigurd Meldgaard <sigurdm@google.com>
2022-03-24 11:56:50 +0000

    lint

--- test/global/activate/git_package_test.dart
+++ test/global/activate/git_package_test.dart
@@ -2,10 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
 
 import '../../descriptor.dart' as d;
-import 'package:path/path.dart' as p;
 import '../../test_pub.dart';
 
 void main() {
