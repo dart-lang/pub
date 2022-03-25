14a3e076eaadd0aca8e07f19a3de604eaf182075
Sigurd Meldgaard <sigurdm@google.com>
2022-03-25 14:13:59 +0000

    Fix typo in expectation

--- test/get/git/path_test.dart
+++ test/get/git/path_test.dart
@@ -97,7 +97,7 @@ void main() {
 
       await pubGet(
         error: contains(
-          'Invalid description in the "myapp" pubspec on the "sub" dependency: The \'path\' field of the description must be a relative path url.',
+          'Invalid description in the "myapp" pubspec on the "sub" dependency: The \'path\' field of the description must be a relative path URL.',
         ),
         exitCode: exit_codes.DATA,
       );
