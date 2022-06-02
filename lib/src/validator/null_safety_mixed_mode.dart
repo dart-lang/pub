0b52e6a86e4af91b81e847045f843acf44b3ee74
Sigurd Meldgaard <sigurdm@google.com>
2022-06-02 09:27:11 +0200

    Remove debug print (#3441)

--- lib/src/validator/null_safety_mixed_mode.dart
+++ lib/src/validator/null_safety_mixed_mode.dart
@@ -31,7 +31,6 @@ class NullSafetyMixedModeValidator extends Validator {
         ),
       ),
     );
-    print(analysisResult.compliance);
     if (analysisResult.compliance == NullSafetyCompliance.mixed) {
       warnings.add('''
 This package is opting into null-safety, but a dependency or file is not.
@@ -39,10 +38,10 @@ This package is opting into null-safety, but a dependency or file is not.
 ${analysisResult.reason}
 
 Note that by publishing with non-migrated dependencies your package may be
-broken at any time if one of your dependencies migrates without a breaking 
-change release. 
+broken at any time if one of your dependencies migrates without a breaking
+change release.
 
-We highly recommend that you wait until all of your dependencies have been 
+We highly recommend that you wait until all of your dependencies have been
 migrated before publishing.
 
 Run `pub outdated --mode=null-safety` for more information about the state of
