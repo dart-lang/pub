3fefe692baf08bfb5e195d94ef07b21cb451df44
Sigurd Meldgaard <sigurdm@google.com>
2022-08-01 11:26:41 +0000

    Remove all support for legacy .packages file

--- lib/src/command/upgrade.dart
+++ lib/src/command/upgrade.dart
@@ -56,9 +56,6 @@ class UpgradeCommand extends PubCommand {
 
     argParser.addFlag('packages-dir', hide: true);
 
-    argParser.addFlag('legacy-packages-file',
-        help: 'Generate the legacy ".packages" file', negatable: false);
-
     argParser.addFlag(
       'major-versions',
       help: 'Upgrades packages to their latest resolvable versions, '
@@ -82,8 +79,6 @@ class UpgradeCommand extends PubCommand {
   bool get _dryRun => argResults['dry-run'];
 
   bool get _precompile => argResults['precompile'];
-
-  bool get _packagesFile => argResults['legacy-packages-file'];
 
   bool get _upgradeNullSafety =>
       argResults['nullsafety'] || argResults['null-safety'];
@@ -131,7 +126,6 @@ class UpgradeCommand extends PubCommand {
       dryRun: _dryRun,
       precompile: _precompile,
       onlyReportSuccessOrFailure: onlySummary,
-      generateDotPackages: _packagesFile,
       analytics: analytics,
     );
     _showOfflineWarning();
@@ -243,7 +237,6 @@ be direct 'dependencies' or 'dev_dependencies', following packages are not:
         dryRun: true,
         precompile: _precompile,
         analytics: null, // No analytics for dry-run
-        generateDotPackages: false,
       );
     } else {
       if (changes.isNotEmpty) {
@@ -256,7 +249,6 @@ be direct 'dependencies' or 'dev_dependencies', following packages are not:
         SolveType.get,
         precompile: _precompile,
         analytics: analytics,
-        generateDotPackages: argResults['legacy-packages-file'],
       );
     }
 
@@ -341,7 +333,6 @@ be direct 'dependencies' or 'dev_dependencies', following packages are not:
         dryRun: true,
         precompile: _precompile,
         analytics: null,
-        generateDotPackages: false,
       );
     } else {
       if (changes.isNotEmpty) {
@@ -354,7 +345,6 @@ be direct 'dependencies' or 'dev_dependencies', following packages are not:
         SolveType.upgrade,
         precompile: _precompile,
         analytics: analytics,
-        generateDotPackages: argResults['legacy-packages-file'],
       );
     }
 
