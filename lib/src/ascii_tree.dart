8119913aa875476f356be4f8e33fa36d1560db55
Sigurd Meldgaard <sigurdm@google.com>
2022-08-26 12:34:01 +0000

    typo

--- lib/src/ascii_tree.dart
+++ lib/src/ascii_tree.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// A simple library for rendering tree-like structures in Unicode symbols wiht
+/// A simple library for rendering tree-like structures in Unicode symbols with
 /// a fallback to ASCII.
 import 'dart:io';
 
