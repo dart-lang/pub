1d69451126b41f02ef09100185d4dca30530e1d5
Sigurd Meldgaard <sigurdm@google.com>
2022-10-04 14:32:54 +0000

    Test more cases

--- test/dart3_sdk_constraint_hack_test.dart
+++ test/dart3_sdk_constraint_hack_test.dart
@@ -37,6 +37,36 @@ void main() {
     );
   });
 
+  test('The bound of ">=2.11.0 <2.999.0" is not modified', () async {
+    await d.dir(appPath, [
+      d.pubspec({
+        'name': 'myapp',
+        'environment': {'sdk': '>=2.11.0 <2.999.0'}
+      }),
+    ]).create();
+
+    await pubGet(
+      error: contains(
+          'Because myapp requires SDK version >=2.11.0 <2.999.0, version solving failed'),
+      environment: {'_PUB_TEST_SDK_VERSION': '3.5.0'},
+    );
+  });
+
+  test('The bound of ">=2.11.0 <3.0.0-0.0" is not modified', () async {
+    await d.dir(appPath, [
+      d.pubspec({
+        'name': 'myapp',
+        'environment': {'sdk': '>=2.11.0 <3.0.0-0.0'}
+      }),
+    ]).create();
+
+    await pubGet(
+      error: contains(
+          'Because myapp requires SDK version >=2.11.0 <3.0.0-0.0, version solving failed'),
+      environment: {'_PUB_TEST_SDK_VERSION': '3.5.0'},
+    );
+  });
+
   test('The bound of ">=2.12.0 <3.0.0" is modified', () async {
     await d.dir(appPath, [
       d.pubspec({
@@ -46,5 +76,36 @@ void main() {
     ]).create();
 
     await pubGet(environment: {'_PUB_TEST_SDK_VERSION': '3.5.0'});
+  });
+
+  test('The bound of ">=2.12.0 <3.0.0-0" is modified', () async {
+    // For the upper bound <3.0.0 is treated as <3.0.0-0, so they both have
+    // the rewrite applied.
+    await d.dir(appPath, [
+      d.pubspec({
+        'name': 'myapp',
+        'environment': {'sdk': '>=2.12.0 <3.0.0-0'}
+      }),
+    ]).create();
+
+    await pubGet(environment: {'_PUB_TEST_SDK_VERSION': '3.5.0'});
+  });
+
+  test(
+      'The bound of ">=2.12.0 <3.0.0" is not compatible with prereleases of dart 4',
+      () async {
+    await d.dir(appPath, [
+      d.pubspec({
+        'name': 'myapp',
+        'environment': {'sdk': '>=2.12.0 <3.0.0'}
+      }),
+    ]).create();
+
+    await pubGet(
+      environment: {'_PUB_TEST_SDK_VERSION': '4.0.0-alpha'},
+      error: contains(
+        'Because myapp requires SDK version >=2.12.0 <4.0.0, version solving failed.',
+      ),
+    );
   });
 }
