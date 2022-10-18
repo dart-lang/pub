a0567760b5c02facf1c485dc0a16f55576ea25b2
Sigurd Meldgaard <sigurdm@google.com>
2022-10-18 10:40:56 +0000

    Fix in dependency_services

--- lib/src/command/dependency_services.dart
+++ lib/src/command/dependency_services.dart
@@ -488,15 +488,15 @@ class DependencyServicesApplyCommand extends PubCommand {
                   // `pub get` gets this version-listing from the downloaded
                   // archive but we don't want to download all archives - so we
                   // copy it from the version listing.
-                  var listedId = (await cache.getVersions(package.toRef()))
+                  package = (await cache.getVersions(package.toRef()))
                       .firstWhere((id) => id == package, orElse: () => package);
-                  if ((listedId.description as ResolvedHostedDescription)
+                  if ((package.description as ResolvedHostedDescription)
                           .sha256 ==
                       null) {
                     // This happens when we resolved a package from a legacy
                     // server not providing archive_sha256. As a side-effect of
                     // downloading the package we compute and store the sha256.
-                    listedId = await cache.downloadPackage(package);
+                    package = await cache.downloadPackage(package);
                   }
                 }
               } else {
