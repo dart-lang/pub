d0fca317a275bcb3e33499ef90d9cd3f1e38ebe4
Sigurd Meldgaard <sigurdm@google.com>
2022-11-21 11:34:34 +0000

    Fix windows path delimitors

--- test/global/activate/missing_git_repo_test.dart
+++ test/global/activate/missing_git_repo_test.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:path/path.dart' as p;
 import 'package:pub/src/exit_codes.dart' as exit_codes;
 import 'package:test/test.dart';
 
@@ -12,10 +13,13 @@ void main() {
   test('fails if the Git repo does not exist', () async {
     ensureGit();
 
+    // Git will report the error message using forward slashes, even on Windows.
+    final pathWithSlashes = p.posix.joinAll([...p.split(sandbox), 'nope.git']);
+
     await runPub(
       args: ['global', 'activate', '-sgit', '../nope.git'],
       error: contains(
-        "'$sandbox/nope.git' does not appear to be a git repository",
+        "'$pathWithSlashes' does not appear to be a git repository",
       ),
       exitCode: exit_codes.UNAVAILABLE,
     );
