6fcd83612baa2328966673860d77b4700c4c8cb8
Sigurd Meldgaard <sigurdm@google.com>
2023-02-14 16:00:16 +0000

    Remove unused import

--- lib/src/source/path.dart
+++ lib/src/source/path.dart
@@ -14,7 +14,6 @@ import '../package_name.dart';
 import '../pubspec.dart';
 import '../source.dart';
 import '../system_cache.dart';
-import '../utils.dart';
 
 /// A package [Source] that gets packages from a given local file path.
 class PathSource extends Source {
