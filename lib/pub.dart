b615e9e1f5c5c525c23b08c0e7ff42599c9dcbea
Sigurd Meldgaard <sigurdm@google.com>
2023-03-30 09:51:51 +0200

    Throw catchable exception from ensurePubspecResolved (#3852)

--- lib/pub.dart
+++ lib/pub.dart
@@ -3,9 +3,12 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:args/command_runner.dart';
+
 import 'src/entrypoint.dart';
+import 'src/exceptions.dart';
 import 'src/pub_embeddable_command.dart';
 import 'src/system_cache.dart';
+
 export 'src/executable.dart'
     show
         getExecutableForCommand,
@@ -39,6 +42,8 @@ Command<int> pubCommand({
 ///
 /// If [onlyOutputWhenTerminal] is `true` (the default) there will be no
 /// output if no terminal is attached.
+///
+/// Throws a [ResolutionFailedException] if resolution fails.
 Future<void> ensurePubspecResolved(
   String dir, {
   PubAnalytics? analytics,
@@ -47,10 +52,19 @@ Future<void> ensurePubspecResolved(
   bool summaryOnly = true,
   bool onlyOutputWhenTerminal = true,
 }) async {
-  await Entrypoint(dir, SystemCache(isOffline: isOffline)).ensureUpToDate(
-    analytics: analytics,
-    checkForSdkUpdate: checkForSdkUpdate,
-    summaryOnly: summaryOnly,
-    onlyOutputWhenTerminal: onlyOutputWhenTerminal,
-  );
+  try {
+    await Entrypoint(dir, SystemCache(isOffline: isOffline)).ensureUpToDate(
+      analytics: analytics,
+      checkForSdkUpdate: checkForSdkUpdate,
+      summaryOnly: summaryOnly,
+      onlyOutputWhenTerminal: onlyOutputWhenTerminal,
+    );
+  } on ApplicationException catch (e) {
+    throw ResolutionFailedException._(e.toString());
+  }
+}
+
+class ResolutionFailedException implements Exception {
+  String message;
+  ResolutionFailedException._(this.message);
 }
