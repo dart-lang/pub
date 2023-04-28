a3f8b2fd36ec432450caf907474a02023ef3e44e
Sigurd Meldgaard <sigurdm@google.com>
2023-04-28 14:59:25 +0200

    Reinterpret dart sdk constraints when read from a lockfile (#3897)
    * Reinterpret dart sdk constraints when read from a lockfile

--- lib/src/pubspec.dart
+++ lib/src/pubspec.dart
@@ -145,56 +145,6 @@ class Pubspec extends PubspecBase {
   /// pubspec.
   final bool _includeDefaultSdkConstraint;
 
-  SdkConstraint _interpretDartSdkConstraint(
-    VersionConstraint originalConstraint, {
-    required VersionConstraint? defaultUpperBoundConstraint,
-  }) {
-    VersionConstraint constraint = originalConstraint;
-    if (defaultUpperBoundConstraint != null &&
-        constraint is VersionRange &&
-        constraint.max == null &&
-        defaultUpperBoundConstraint.allowsAny(constraint)) {
-      constraint = VersionConstraint.intersection(
-        [constraint, defaultUpperBoundConstraint],
-      );
-    }
-    // If a package is null safe it should also be compatible with dart 3.
-    // Therefore we rewrite a null-safety enabled constraint with the upper
-    // bound <3.0.0 to be have upper bound <4.0.0
-    //
-    // Only do this rewrite after dart 3.
-    if (sdk.version.major >= 3 &&
-        constraint is VersionRange &&
-        LanguageVersion.fromSdkConstraint(constraint) >=
-            LanguageVersion.firstVersionWithNullSafety &&
-        // <3.0.0 is parsed into a max of 3.0.0-0, so that is what we look for
-        // here.
-        constraint.max == Version(3, 0, 0).firstPreRelease &&
-        constraint.includeMax == false) {
-      constraint = VersionRange(
-        min: constraint.min,
-        includeMin: constraint.includeMin,
-        // We don't have to use .firstPreRelease as the constructor will do that
-        // if needed.
-        max: Version(4, 0, 0),
-      );
-    }
-    return SdkConstraint(constraint, originalConstraint: originalConstraint);
-  }
-
-  // Flutter constraints get special treatment, as Flutter won't be using
-  // semantic versioning to mark breaking releases. We simply ignore upper
-  // bounds.
-  SdkConstraint _interpretFlutterSdkConstraint(VersionConstraint constraint) {
-    if (constraint is VersionRange) {
-      return SdkConstraint(
-        VersionRange(min: constraint.min, includeMin: constraint.includeMin),
-        originalConstraint: constraint,
-      );
-    }
-    return SdkConstraint(constraint);
-  }
-
   /// Parses the "environment" field in [parent] and returns a map from SDK
   /// identifiers to constraints on those SDKs.
   Map<String, SdkConstraint> _parseEnvironment(YamlMap parent) {
@@ -215,7 +165,7 @@ class Pubspec extends PubspecBase {
       );
     }
     var constraints = {
-      'dart': _interpretDartSdkConstraint(
+      'dart': SdkConstraint.interpretDartSdkConstraint(
         originalDartSdkConstraint,
         defaultUpperBoundConstraint: _includeDefaultSdkConstraint
             ? _defaultUpperBoundSdkConstraint
@@ -239,7 +189,7 @@ class Pubspec extends PubspecBase {
           _FileType.pubspec,
         );
         constraints[name] = name == 'flutter'
-            ? _interpretFlutterSdkConstraint(constraint)
+            ? SdkConstraint.interpretFlutterSdkConstraint(constraint)
             : SdkConstraint(constraint);
       });
     }
@@ -668,6 +618,62 @@ class SdkConstraint {
     this.effectiveConstraint, {
     VersionConstraint? originalConstraint,
   }) : originalConstraint = originalConstraint ?? effectiveConstraint;
+
+  /// Implement support for down to 2.12 in the dart 3 series. Note that this
+  /// function has to be be idempotent, because we apply it both when we write
+  /// and read lock-file constraints, so applying this function a second time
+  /// should have no further effect.
+  factory SdkConstraint.interpretDartSdkConstraint(
+    VersionConstraint originalConstraint, {
+    required VersionConstraint? defaultUpperBoundConstraint,
+  }) {
+    VersionConstraint constraint = originalConstraint;
+    if (defaultUpperBoundConstraint != null &&
+        constraint is VersionRange &&
+        constraint.max == null &&
+        defaultUpperBoundConstraint.allowsAny(constraint)) {
+      constraint = VersionConstraint.intersection(
+        [constraint, defaultUpperBoundConstraint],
+      );
+    }
+    // If a package is null safe it should also be compatible with dart 3.
+    // Therefore we rewrite a null-safety enabled constraint with the upper
+    // bound <3.0.0 to be have upper bound <4.0.0
+    //
+    // Only do this rewrite after dart 3.
+    if (sdk.version.major >= 3 &&
+        constraint is VersionRange &&
+        LanguageVersion.fromSdkConstraint(constraint) >=
+            LanguageVersion.firstVersionWithNullSafety &&
+        // <3.0.0 is parsed into a max of 3.0.0-0, so that is what we look for
+        // here.
+        constraint.max == Version(3, 0, 0).firstPreRelease &&
+        constraint.includeMax == false) {
+      constraint = VersionRange(
+        min: constraint.min,
+        includeMin: constraint.includeMin,
+        // We don't have to use .firstPreRelease as the constructor will do that
+        // if needed.
+        max: Version(4, 0, 0),
+      );
+    }
+    return SdkConstraint(constraint, originalConstraint: originalConstraint);
+  }
+
+  // Flutter constraints get special treatment, as Flutter won't be using
+  // semantic versioning to mark breaking releases. We simply ignore upper
+  // bounds.
+  factory SdkConstraint.interpretFlutterSdkConstraint(
+    VersionConstraint constraint,
+  ) {
+    if (constraint is VersionRange) {
+      return SdkConstraint(
+        VersionRange(min: constraint.min, includeMin: constraint.includeMin),
+        originalConstraint: constraint,
+      );
+    }
+    return SdkConstraint(constraint);
+  }
 
   /// The language version of a constraint is determined from how it is written.
   LanguageVersion get languageVersion =>
