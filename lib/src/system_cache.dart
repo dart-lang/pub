70f268246b89dd8c814603734e84da79d13c8280
Sigurd Meldgaard <sigurdm@google.com>
2023-05-23 12:01:26 +0200

    Update lib/src/system_cache.dart
    Co-authored-by: Jonas Finnemann Jensen <jopsen@gmail.com>

--- lib/src/system_cache.dart
+++ lib/src/system_cache.dart
@@ -345,7 +345,7 @@ See https://dart.dev/resources/dart-3-migration#other-tools-changes for details.
         writeTextFile(legacyCacheDeprecatedFile, '''
 This pub cache is no longer used by recent Dart/Flutter releases.
 
-Consider deleting it.
+Consider deleting it, if you are not using Dart versions earlier than 2.8.0.
 
 See https://dart.dev/resources/dart-3-migration#other-tools-changes for details.
 ''');
