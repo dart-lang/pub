6f82ea77ef309f25a35dbac165c0f5c1566fde75
Sarah Zakarias <zarah@google.com>
2023-11-20 13:15:26 +0100

    extra test

--- test/outdated/outdated_test.dart
+++ test/outdated/outdated_test.dart
@@ -191,6 +191,27 @@ Future<void> main() async {
     await ctx.runOutdatedTests();
   });
 
+  testWithGolden("don't show retracted", (ctx) async {
+    final builder = await servePackages();
+    builder
+      ..serve('foo', '1.0.0', deps: {'transitive': '^1.0.0'})
+      ..serve('transitive', '1.2.3');
+
+    await d.dir(appPath, [
+      d.pubspec({
+        'name': 'app',
+        'dependencies': {
+          'foo': '^1.0.0',
+        },
+      }),
+    ]).create();
+    await pubGet();
+    builder.retractPackageVersion('foo', '1.0.0');
+    builder.serve('foo', '1.2.0');
+    await pubUpgrade();
+    await ctx.runOutdatedTests();
+  });
+
   testWithGolden('show discontinued and retracted', (ctx) async {
     final builder = await servePackages();
     builder
