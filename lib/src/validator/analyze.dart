5653aaa32064f8ebe6a0c31a811e6a8c9337e950
Daco Harkes <dacoharkes@google.com>
2023-12-05 16:54:36 +0100

    Analyze `build.dart` and `link.dart`

--- lib/src/validator/analyze.dart
+++ lib/src/validator/analyze.dart
@@ -14,17 +14,22 @@ import '../validator.dart';
 
 /// Runs `dart analyze` and gives a warning if it returns non-zero.
 class AnalyzeValidator extends Validator {
-  // Only analyze dart code in the following sub-folders.
-  static const List<String> _dirsToAnalyze = ['lib', 'bin'];
+  // Only analyze dart code in the following sub-folders and files.
+  static const List<String> _entriesToAnalyze = [
+    'bin',
+    'lib',
+    'build.dart',
+    'link.dart',
+  ];
 
   @override
   Future<void> validate() async {
-    final dirs = _dirsToAnalyze
+    final entries = _entriesToAnalyze
         .map((dir) => p.join(entrypoint.rootDir, dir))
-        .where(dirExists);
+        .where(entryExists);
     final result = await runProcess(
       Platform.resolvedExecutable,
-      ['analyze', ...dirs, p.join(entrypoint.rootDir, 'pubspec.yaml')],
+      ['analyze', ...entries, p.join(entrypoint.rootDir, 'pubspec.yaml')],
     );
     if (result.exitCode != 0) {
       final limitedOutput = limitLength(result.stdout.join('\n'), 1000);
