0482d609fa88cf3050e2793ea24629b6ead5b7df
Sigurd Meldgaard <sigurdm@google.com>
2024-02-08 15:41:52 +0000

    Validator for devtools plugins

---
+++ lib/src/validator/devtools_plugin.dart
@@ -0,0 +1,46 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+import 'package:path/path.dart' as p;
+
+import '../io.dart';
+import '../validator.dart';
+
+//   If the `extension/devtools/` directory exists
+//  verify the directory contains a file `config.yaml` and a non-empty `build/` directory
+class DevtoolsPluginValidator extends Validator {
+  static String docRef = 'See https://dart.dev/tools/pub/package-layout.';
+
+  @override
+  Future<void> validate() async {
+    if (dirExists(p.join(entrypoint.rootDir, 'extension', 'devtools'))) {
+      if (!files.any(
+            (f) => p.equals(
+              f,
+              p.join(
+                entrypoint.rootDir,
+                'extension',
+                'devtools',
+                'config.yaml',
+              ),
+            ),
+          ) ||
+          !files.any(
+            (f) => p.isWithin(
+              p.join(entrypoint.rootDir, 'extension', 'devtools', 'build'),
+              f,
+            ),
+          )) {
+        warnings.add('''
+It looks like you are making a devtools extension!
+
+The folder `extensions/devtools` should contain both a
+* `config.yaml` file and a
+* non-empty `build` director'
+''');
+      }
+    }
+  }
+}
---
+++ test/validator/devtools_plugin_test.dart
@@ -0,0 +1,52 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:test/test.dart';
+
+import '../descriptor.dart' as d;
+import '../test_pub.dart';
+import 'utils.dart';
+
+void main() {
+  test('No warning if both are provided', () async {
+    await d.validPackage().create();
+    await d.dir(appPath, [
+      d.dir('extension', [
+        d.dir('devtools', [
+          d.file('config.yaml'),
+          d.dir('build', [d.file('some_file')]),
+        ]),
+      ]),
+    ]).create();
+    await expectValidation();
+  });
+  test('warns if build-directory is empty', () async {
+    await d.validPackage().create();
+    await d.dir(appPath, [
+      d.dir('extension', [
+        d.dir('devtools', [
+          d.file('config.yaml'),
+          d.dir('build', []),
+        ]),
+      ]),
+    ]).create();
+    await expectValidationWarning(
+      'The folder `extensions/devtools` should contain both a',
+    );
+  });
+
+  test('warns if config.yaml is missing', () async {
+    await d.validPackage().create();
+    await d.dir(appPath, [
+      d.dir('extension', [
+        d.dir('devtools', [
+          d.dir('build', [d.file('some_file')]),
+        ]),
+      ]),
+    ]).create();
+    await expectValidationWarning(
+      'The folder `extensions/devtools` should contain both a',
+    );
+  });
+}
