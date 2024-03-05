bc47348956d42263a58e25571e8d7aee4527f574
Sigurd Meldgaard <sigurdm@google.com>
2024-03-05 13:11:08 +0000

    Add debug prints

--- lib/src/entrypoint.dart
+++ lib/src/entrypoint.dart
@@ -694,6 +694,7 @@ To update `$lockFilePath` run `$topLevelProgram pub get`$suffix without
       log.fine('No $packageConfigPath file found".\n');
       return false;
     }
+    log.fine('$packageConfigPath $packageConfigStat'); // XXX remove
     final flutter = FlutterSdk();
     // If Flutter has moved since last invocation, we want to have new
     // sdk-packages, and therefore do a new resolution.
@@ -728,6 +729,7 @@ To update `$lockFilePath` run `$topLevelProgram pub get`$suffix without
       log.fine('No $lockFilePath file found.');
       return false;
     }
+    log.fine('$lockFilePath $lockFileStat'); // XXX remove
 
     final lockFileModified = lockFileStat.modified;
     var lockfileNewerThanPubspecs = true;
@@ -753,6 +755,8 @@ To update `$lockFilePath` run `$topLevelProgram pub get`$suffix without
         // A dependency is missing - do a full new resolution.
         return false;
       }
+      log.fine('$pubspecPath $pubspecStat'); // XXX remove
+
       if (pubspecStat.modified.isAfter(lockFileModified)) {
         log.fine('`$pubspecPath` is newer than `$lockFilePath`');
         lockfileNewerThanPubspecs = false;
