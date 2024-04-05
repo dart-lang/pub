7bba901fe81489b0ac57b3e7444eb0da760a4e3d
Sigurd Meldgaard <sigurdm@google.com>
2024-04-05 12:25:41 +0000

    Filter content hashes from golden files

--- test/test_pub.dart
+++ test/test_pub.dart
@@ -936,8 +936,12 @@ String filterUnstableText(String input) {
   // Any paths in output should be relative to the sandbox and with forward
   // slashes to be stable across platforms.
   input = input.replaceAll(d.sandbox, r'$SANDBOX');
-  input =
-      input.replaceAllMapped(RegExp(r'\\(\S|\.)'), (match) => '/${match[1]}');
+  input = input
+      .replaceAllMapped(RegExp(r'\\(\S|\.)'), (match) => '/${match[1]}')
+      .replaceAll(
+        RegExp(r'sha256: "?[0-9a-f]{64}"?', multiLine: true),
+        r'sha256: $SHA256',
+      );
   var port = _globalServer?.port;
   if (port != null) {
     input = input.replaceAll(port.toString(), '\$PORT');
