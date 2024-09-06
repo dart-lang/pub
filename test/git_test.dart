6b25eb852342ea7c09bb1f155c42431629f941a5
Sigurd Meldgaard <sigurdm@google.com>
2024-09-06 11:20:38 +0000

    Lints

--- test/git_test.dart
+++ test/git_test.dart
@@ -2,10 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
 import 'dart:typed_data';
 
-import 'package:pub/src/error_group.dart';
 import 'package:pub/src/git.dart';
 import 'package:test/test.dart';
 
