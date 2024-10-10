42017cf9612515b1fd55f48b14933eb961ba9e9c
Sigurd Meldgaard <sigurdm@google.com>
2024-10-10 13:17:10 +0000

    Validate lockfile before recompiling executable

--- test/global/run/recompiles_if_snapshot_is_out_of_date_test.dart
+++ test/global/run/recompiles_if_snapshot_is_out_of_date_test.dart
@@ -3,7 +3,9 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:path/path.dart' as p;
+import 'package:pub/src/exit_codes.dart';
 import 'package:pub/src/io.dart';
+import 'package:pub/src/sdk/sdk_package_config.dart';
 import 'package:test/test.dart';
 
 import '../../descriptor.dart' as d;
@@ -58,5 +60,92 @@ void main() {
         ]),
       ]),
     ]).validate();
+  });
+
+  test('validate resolution before recompilation', () async {
+    final server = await servePackages();
+    server.serve(
+      'foo',
+      '1.0.0',
+      deps: {
+        'bar': {'sdk': 'dart', 'version': '^1.0.0'},
+      },
+      contents: [
+        d.dir('bin', [
+          d.file('foo.dart', 'main() => print("foo");'),
+        ]),
+      ],
+    );
+
+    await d.dir('dart', [
+      d.dir('packages', [
+        d.dir('bar', [
+          d.libPubspec('bar', '1.0.0', deps: {}),
+        ]),
+      ]),
+      d.sdkPackagesConfig(
+        SdkPackageConfig(
+          'dart',
+          {'bar': SdkPackage('bar', 'packages/bar')},
+          1,
+        ),
+      ),
+    ]).create();
+
+    await runPub(
+      args: ['global', 'activate', 'foo'],
+      environment: {'DART_ROOT': p.join(d.sandbox, 'dart')},
+    );
+
+    await runPub(
+      args: ['global', 'run', 'foo'],
+      environment: {'DART_ROOT': p.join(d.sandbox, 'dart')},
+      output: 'foo',
+    );
+
+    await d.dir('dart', [
+      d.dir('packages', [
+        d.dir('bar', [
+          // Within constraint, but doesn't satisfy pubspec.lock.
+          d.libPubspec('bar', '1.2.0', deps: {}),
+        ]),
+      ]),
+    ]).create();
+
+    await runPub(
+      args: ['global', 'run', 'foo'],
+      environment: {
+        'DART_ROOT': p.join(d.sandbox, 'dart'),
+        '_PUB_TEST_SDK_VERSION': '3.2.1+4',
+      },
+      error: allOf(
+        contains('The package `foo` as currently activated cannot resolve to '
+            'the same packages'),
+        contains('Try reactivating the package'),
+      ),
+      exitCode: DATA,
+    );
+
+    await d.dir('dart', [
+      d.dir('packages', [
+        d.dir('bar', [
+          // Doesn't fulfill constraint, but doesn't satisfy pubspec.lock.
+          d.libPubspec('bar', '2.0.0', deps: {}),
+        ]),
+      ]),
+    ]).create();
+    await runPub(
+      args: ['global', 'run', 'foo'],
+      environment: {
+        'DART_ROOT': p.join(d.sandbox, 'dart'),
+        '_PUB_TEST_SDK_VERSION': '3.2.1+4',
+      },
+      error: allOf(
+        contains('Because every version of foo depends on bar ^1.0.0 from sdk'),
+        contains('The package `foo` as currently activated cannot resolve.'),
+        contains('Try reactivating the package'),
+      ),
+      exitCode: 1,
+    );
   });
 }
