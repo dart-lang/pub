d464e12bb01d2692dedc29dab3c38f8e116342d5
Sigurd Meldgaard <sigurdm@google.com>
2026-02-23 12:43:54 +0000

    Add explicit regression test

--- test/git_test.dart
+++ test/git_test.dart
@@ -2,10 +2,14 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:pub/src/git.dart';
 import 'package:test/test.dart';
+
+import 'test_pub.dart';
+import 'descriptor.dart';
 
 void main() {
   test('splitZeroTerminated works', () {
@@ -43,5 +47,29 @@ void main() {
       ),
       throwsA(isA<FormatException>()),
     );
+  });
+
+  test('safe.bareRepository is enabled by default in test environment', () {
+    // Regression protection for https://github.com/dart-lang/pub/issues/4770.
+    final r = Process.runSync('git', [
+      'config',
+      '--get',
+      'safe.bareRepository',
+    ], environment: getPubTestEnvironment());
+    expect(r.stdout, contains('explicit'));
+    Process.runSync(
+      'git',
+      ['init', '--bare'],
+      workingDirectory: sandbox,
+      environment: getPubTestEnvironment(),
+    );
+    final r1 = Process.runSync(
+      'git',
+      ['log'],
+      workingDirectory: sandbox,
+      environment: getPubTestEnvironment(),
+    );
+    expect(r1.exitCode, isNot(0));
+    expect(r1.stderr, contains('fatal: cannot use bare repository '));
   });
 }
