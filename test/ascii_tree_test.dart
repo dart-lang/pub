0b1ffff96748f523decc76e2f2bf4588fcbfa8e3
Jonas Finnemann Jensen <jonasfj@google.com>
2026-02-23 10:39:22 +0100

    More test annotations

--- test/ascii_tree_test.dart
+++ test/ascii_tree_test.dart
@@ -2,6 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+@TestOn('vm')
+library;
+
 import 'package:pub/src/ascii_tree.dart' as tree;
 import 'package:pub/src/package.dart';
 import 'package:pub/src/pubspec.dart';
--- test/help_test.dart
+++ test/help_test.dart
@@ -2,8 +2,12 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+@TestOn('vm')
+library;
+
 import 'package:args/command_runner.dart';
 import 'package:pub/src/command_runner.dart' show PubCommandRunner;
+import 'package:test/test.dart';
 
 import 'golden_file.dart';
 
